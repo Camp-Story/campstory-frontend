9122ceda2855b12742da19c4231f2e843e100530
soyoung125 <sypark041754@gmail.com>
2025-01-20 15:08:11 +0900

    chore: tailwind setting

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,12 @@ function App() {
         <input placeholder="password" name="password" type="password" />
         <button type="submit">submit</button>
       </form>
+      <div className="text-highlight">강조조</div>
+      <div className="text-banner">배너너</div>
+      <div className="text-title">제목</div>
+      <div className="text-sub-title">서브타이틀</div>
+      <div className="text-body">본문</div>
+      <div className="text-explain">설명</div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-orange": {
+          300: "#F29B30",
+          500: "#F85900",
+        },
+        "primary-green": {
+          500: "#1CA673",
+        },
+        "primary-blue": {
+          500: "#1A9EFE",
+        },
+        gray: {
+          0: "#FFFFFF",
+          100: "#EAEAEA",
+          200: "#B4B4B4",
+          300: "#676767",
+          400: "#333333",
+          500: "#191919",
+        },
+      },
+      fontSize: {
+        highlight: ["40px", { linHeight: "auto", fontWeight: 700 }],
+        banner: ["32px", { linHeight: "auto", fontWeight: 400 }],
+        title: ["32px", { linHeight: "auto", fontWeight: 600 }],
+        "sub-title": ["20px", { linHeight: "auto", fontWeight: 500 }],
+        body: ["15px", { linHeight: "auto", fontWeight: 400 }],
+        explain: ["13px", { linHeight: "auto", fontWeight: 400 }],
+      },
+    },
   },
   plugins: [],
 };
