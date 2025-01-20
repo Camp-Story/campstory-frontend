c992a02a7b18dca73575fec63351641d617660b0
soyoung125 <sypark041754@gmail.com>
2025-01-21 00:41:01 +0900

    refactor: update path

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -15,30 +15,40 @@ export const PATH = {
   camping: "/camping",
   campingSearch: "/camping/search",
   campingInfo: (id: string) => `/camping/information/${id}`,
+  campingInfoPath: "/camping/information/:id",
   campingReservation: (id: string) => `/camping/reservation/${id}`,
+  campingReservationPath: "/camping/reservation/:id",
 
   // event
   evnet: "/evnet",
   evnetSearch: "/evnet/search",
   evnetInfo: (id: string) => `/evnet/information/${id}`,
+  eventInfoPath: "/evnet/information/:id",
 
   // restaurant
   restaurant: "/restaurant",
   restaurantSearch: "/restaurant/search",
   restaurantInfo: (id: string) => `/restaurant/information/${id}`,
+  restaurantInfoPath: "/restaurant/information/:id",
 
   // shopping
   shopping: "/shopping",
   shoppingSearch: "/shopping/search",
   shoppingInfo: (id: string) => `/shopping/information/${id}`,
+  shoppingInfoPath: "/shopping/information/:id",
 
   // community
   community: "/community",
   communityPost: (id: string) => `/community/${id}`,
+  communityPostPath: "/community/:id",
   communityCreate: "/community/create",
   communityModify: (id: string) => `/community/modify/${id}`,
+  communityModifyPath: "/community/modify/:id",
+
   question: "/question",
   questionPost: (id: string) => `/question/${id}`,
+  questionPostPath: "/question/:id",
   questionCreate: "/question/create",
   questionModify: (id: string) => `/question/modify/${id}`,
+  questionModifyPath: "/question/modify/:id",
 };
