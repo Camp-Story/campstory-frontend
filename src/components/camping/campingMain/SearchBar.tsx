afb55dc0715a510f4b5b007844d5ddcc84e6a60f
marie <marie061313@gmail.com>
2025-01-22 14:36:17 +0900

    feat: SearchBar 컴포넌트 구현

---
+++ src/components/camping/campingMain/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+interface SearchBarProps {
+  className: string;
+  backgroundColor?: boolean;
+}
+
+export default function SearchBar({ className, backgroundColor }: SearchBarProps) {
+  const [focus, setFocus] = useState(false);
+  return (
+    <div className={className}>
+      <input
+        placeholder="검색어를 입력하세요"
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        className={`py-3 px-6 w-[800px] rounded-full focus:outline-green-700 ${
+          backgroundColor
+            ? focus
+              ? "bg-white placeholder-slate-600"
+              : "bg-slate-200 placeholder-slate-600"
+            : "bg-white"
+        } `}
+      />
+    </div>
+  );
+}
