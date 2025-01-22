cf141b40701d4e5f5d0366c4af22593a9b99034c
soyoung125 <sypark041754@gmail.com>
2025-01-22 14:10:42 +0900

    feat: apply path to navigation link

--- src/components/layouts/MainNavigation.tsx
+++ src/components/layouts/MainNavigation.tsx
@@ -1,34 +1,35 @@
+import { PATH } from "@constants/path";
 import { Link } from "react-router";
 
 export default function MainNavigation() {
   return (
     <nav className="flex items-center justify-between">
       <h1>
-        <Link to="/">
+        <Link to={PATH.home}>
           <img src="/images/campstory-logo.png" />
         </Link>
       </h1>
       <ul className="flex gap-[40px] text-body1">
         <li>
-          <Link to="/">캠핑</Link>
+          <Link to={PATH.camping}>캠핑</Link>
         </li>
         <li>
-          <Link to="/">행사</Link>
+          <Link to={PATH.event}>행사</Link>
         </li>
         <li>
-          <Link to="/">음식점</Link>
+          <Link to={PATH.restaurant}>음식점</Link>
         </li>
         <li>
-          <Link to="/">쇼핑몰</Link>
+          <Link to={PATH.shopping}>쇼핑몰</Link>
         </li>
         <li>
-          <Link to="/">커뮤니티</Link>
+          <Link to={PATH.community}>커뮤니티</Link>
         </li>
       </ul>
       <div className="flex gap-[40px] items-center">
-        <Link to="/">로그인</Link>
+        <Link to={PATH.login}>로그인</Link>
         <Link
-          to="/"
+          to={PATH.signUp}
           className="w-24 bg-primary-500 text-center px-5 py-2 rounded text-gray-scale-0"
         >
           회원가입
