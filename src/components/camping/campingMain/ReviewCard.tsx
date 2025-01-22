3d9b428e25055d8f06c33b53343d0c0987c12e83
marie <marie061313@gmail.com>
2025-01-22 22:56:34 +0900

    design: ReviewCard hover 스타일 추가

--- src/components/camping/campingMain/ReviewCard.tsx
+++ src/components/camping/campingMain/ReviewCard.tsx
@@ -4,7 +4,7 @@ export default function ReviewCard({ src, contents, timestamp, userId }: ReviewC
   return (
     <div className="flex gap-2">
       <img src={src} alt="reviewImg" className="w-[200px] h-[200px]" />
-      <div className="flex flex-col gap-2 border-2 border-slate-100 rounded-xl w-[400px] h-[200px] p-6">
+      <div className="flex flex-col gap-2 border-2 border-slate-100 rounded-xl w-[400px] h-[200px] p-6 hover:border-primary-500">
         <div className="text-gray-scale-400 text-body1 leading-5">{contents}</div>
         <div className="text-gray-scale-300 text-body1">{timestamp}</div>
         <div className="flex flex-row gap-2 items-center mt-5">
