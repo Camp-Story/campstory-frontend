a9fbca45875c218343b446f2d7c3db3aa2ef64a0
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 14:13:26 +0900

    add: SubTitie_component

--- src/components/event_components/SubTitle.tsx
+++ src/components/event_components/SubTitle.tsx
@@ -1,17 +1,13 @@
 interface SubTitleProps {
-    mainText: string;
-    subText: string;
+  mainText: string;
+  subText: string;
 }
 
 export default function SubTitle({ mainText, subText }: SubTitleProps) {
-    return (
-        <div className="mb-4 mt-[60px]">
-            <div className="text-highlight text-gray-scale-400 font-impact">
-                {mainText}
-            </div>
-            <div className="mb-[20px] text-[20px] text-gray-scale-300">
-                {subText}
-            </div>
-        </div>
-    );
+  return (
+    <div className="mb-4 mt-[60px]">
+      <div className="text-highlight text-gray-scale-400 font-impact">{mainText}</div>
+      <div className="mb-[20px] text-[20px] text-gray-scale-300">{subText}</div>
+    </div>
+  );
 }
