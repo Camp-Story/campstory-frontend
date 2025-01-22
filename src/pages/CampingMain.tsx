bc3e96b4c1fd8ddb5a33a64e4969f82d66031de4
marie <marie061313@gmail.com>
2025-01-22 14:51:58 +0900

    feat: CategoryCard 컴포넌트 구현하기

--- src/pages/CampingMain.tsx
+++ src/pages/CampingMain.tsx
@@ -1,12 +1,19 @@
 import Banner from "@components/camping/campingMain/Banner";
+import CategoryCard from "@components/camping/campingMain/CategoryCard";
 import Subtitle from "@components/camping/campingMain/Subtitle";
 
 export default function CampingMain() {
   return (
-    <div>
+    <div className="flex flex-col gap-[60px]">
       <Banner />
       <div>
         <Subtitle>카테고리별 캠핑장</Subtitle>
+        <div className="flex gap-2 justify-between items-center">
+          <CategoryCard src="/images/camping/camping-category-1.png">캠핑</CategoryCard>
+          <CategoryCard src="/images/camping/camping-category-2.png">글램핑</CategoryCard>
+          <CategoryCard src="/images/camping/camping-category-3.png">카라반</CategoryCard>
+          <CategoryCard src="/images/camping/camping-category-4.png">오토캠핑</CategoryCard>
+        </div>
       </div>
     </div>
   );
