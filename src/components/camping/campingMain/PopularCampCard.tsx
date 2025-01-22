ab08d9fc2d4386e3d293120032fde292d1f69f42
marie <marie061313@gmail.com>
2025-01-22 22:53:04 +0900

    feat: PopularCampCard 클릭시 페이지 이동 구현

--- src/components/camping/campingMain/PopularCampCard.tsx
+++ src/components/camping/campingMain/PopularCampCard.tsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router";
 import PopularCampCardProps from "types/PopularCampingCardProps";
 
-export default function PopularCampCard({ src, category, name }: PopularCampCardProps) {
+export default function PopularCampCard({ src, category, name, path }: PopularCampCardProps) {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div onClick={() => navigate(path)}>
       <img src={src} alt="CampingImage" className="w-[300px] hover:brightness-75" />
       <div className="text-body1 font-bold mt-4 mb-[4px] text-gray-scale-300">{category}</div>
       <div className="text-sub-title font-extrabold text-gray-scale-400">{name}</div>
