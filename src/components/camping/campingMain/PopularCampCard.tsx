2e05ab3d12406623027c48759c5f6abc9e796bc7
marie <marie061313@gmail.com>
2025-01-22 22:35:50 +0900

    design: PopularCampCard hover 스타일 추가

--- src/components/camping/campingMain/PopularCampCard.tsx
+++ src/components/camping/campingMain/PopularCampCard.tsx
@@ -3,7 +3,7 @@ import PopularCampCardProps from "types/PopularCampingCardProps";
 export default function PopularCampCard({ src, category, name }: PopularCampCardProps) {
   return (
     <div>
-      <img src={src} alt="CampingImage" className="w-[300px]" />
+      <img src={src} alt="CampingImage" className="w-[300px] hover:brightness-75" />
       <div className="text-body1 font-bold mt-4 mb-[4px] text-gray-scale-300">{category}</div>
       <div className="text-sub-title font-extrabold text-gray-scale-400">{name}</div>
     </div>
