77d99ab0e38a195326725fa19e0c56686676a212
marie <marie061313@gmail.com>
2025-01-22 15:11:41 +0900

    feat: PopularCampingCard 컴포넌트 구현

--- src/pages/CampingMain.tsx
+++ src/pages/CampingMain.tsx
@@ -1,5 +1,6 @@
 import Banner from "@components/camping/campingMain/Banner";
 import CategoryCard from "@components/camping/campingMain/CategoryCard";
+import PopularCampCard from "@components/camping/campingMain/PopularCampCard";
 import Subtitle from "@components/camping/campingMain/Subtitle";
 
 export default function CampingMain() {
@@ -13,6 +14,23 @@ export default function CampingMain() {
           <CategoryCard src="/images/camping/camping-category-2.png">글램핑</CategoryCard>
           <CategoryCard src="/images/camping/camping-category-3.png">카라반</CategoryCard>
           <CategoryCard src="/images/camping/camping-category-4.png">오토캠핑</CategoryCard>
+        </div>
+      </div>
+      <div>
+        <Subtitle>인기 캠핑장</Subtitle>
+        <div className="flex gap-2 justify-between items-center">
+          <PopularCampCard src="/images/camping/PopularCamping1.png" category="글램핑">
+            스토리 캠핑장
+          </PopularCampCard>
+          <PopularCampCard src="/images/camping/PopularCamping2.png" category="카라반">
+            스토리 캠핑장
+          </PopularCampCard>
+          <PopularCampCard src="/images/camping/PopularCamping3.png" category="오토캠핑">
+            스토리 캠핑장
+          </PopularCampCard>
+          <PopularCampCard src="/images/camping/PopularCamping4.png" category="글램핑">
+            스토리 캠핑장
+          </PopularCampCard>
         </div>
       </div>
     </div>
