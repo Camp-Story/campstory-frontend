f4bf4133ca7f48f2bc04d3c26f6914bba2ddec9b
soyoung125 <sypark041754@gmail.com>
2025-01-22 17:02:14 +0900

    feat: add camping search filter

---
+++ src/constants/codes.ts
@@ -0,0 +1,87 @@
+export const AREA_CODE = [
+  {
+    code: 11,
+    label: "서울특별시",
+    shorten: "서울",
+  },
+  {
+    code: 26,
+    label: "부산광역시",
+    shorten: "부산",
+  },
+  {
+    code: 27,
+    label: "대구광역시",
+    shorten: "대구",
+  },
+  {
+    code: 28,
+    label: "인천광역시",
+    shorten: "인천",
+  },
+  {
+    code: 29,
+    label: "광주광역시",
+    shorten: "광주",
+  },
+  {
+    code: 30,
+    label: "대전광역시",
+    shorten: "대전",
+  },
+  {
+    code: 31,
+    label: "울산광역시",
+    shorten: "울산",
+  },
+  {
+    code: 36,
+    label: "세종특별자치시",
+    shorten: "세종",
+  },
+  {
+    code: 41,
+    label: "경기도",
+    shorten: "경기도",
+  },
+  {
+    code: 43,
+    label: "충청북도",
+    shorten: "충북",
+  },
+  {
+    code: 44,
+    label: "충청남도",
+    shorten: "충남",
+  },
+  {
+    code: 46,
+    label: "전라남도",
+    shorten: "전남",
+  },
+  {
+    code: 47,
+    label: "경상북도",
+    shorten: "경북",
+  },
+  {
+    code: 48,
+    label: "경상남도",
+    shorten: "경남",
+  },
+  {
+    code: 50,
+    label: "제주특별자치도",
+    shorten: "제주",
+  },
+  {
+    code: 51,
+    label: "강원특별자치도",
+    shorten: "강원도",
+  },
+  {
+    code: 52,
+    label: "전북특별자치도",
+    shorten: "전북",
+  },
+];
