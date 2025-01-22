ad7ff4c0327dc649b5f9099912f0f0ee7529c986
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 12:06:35 +0900

    add: EventMainPage add

--- src/pages/EventMain.tsx
+++ src/pages/EventMain.tsx
@@ -1,25 +1,27 @@
-import Banner from "@components/event_components/Banner"
-import CardButton from "@components/event_components/CardButton"
-import EventCalendarSection from "@components/event_components/EventCalendarSection"
-import SubTitle from "@components/event_components/SubTitle"
-import TodaySection from "@components/event_components/TodaySection"
+import Banner from "@components/event_components/Banner";
+import CardButton from "@components/event_components/CardButton";
+import EventCalendarSection from "@components/event_components/EventCalendarSection";
+import SubTitle from "@components/event_components/SubTitle";
+import TodaySection from "@components/event_components/TodaySection";
 
 export default function EventMain() {
-
-    return (
-        <div>
-            <Banner/>
-            <div>
-            <SubTitle mainText="다양한 축제, 새로운 즐거움" subText="음식, 음악, 자연, 문화가 어우러진 축제의 세계를 만나보세요"/>
-                <div className="flex justify-between items-center">
-                    <CardButton src="/images/festival/event-category-1.png">일반</CardButton>
-                    <CardButton src="/images/festival/event-category-2.png">문화 관광</CardButton>
-                    <CardButton src="/images/festival/event-category-3.png">박람회</CardButton>
-                    <CardButton src="/images/festival/event-category-4.png">전시회</CardButton>
-                </div>
-            </div>
-            <TodaySection/>
-            <EventCalendarSection/>
+  return (
+    <div>
+      <Banner />
+      <div>
+        <SubTitle
+          mainText="다양한 축제, 새로운 즐거움"
+          subText="음식, 음악, 자연, 문화가 어우러진 축제의 세계를 만나보세요"
+        />
+        <div className="flex justify-between items-center">
+          <CardButton src="/images/festival/event-category-1.png">일반</CardButton>
+          <CardButton src="/images/festival/event-category-2.png">문화 관광</CardButton>
+          <CardButton src="/images/festival/event-category-3.png">박람회</CardButton>
+          <CardButton src="/images/festival/event-category-4.png">전시회</CardButton>
         </div>
-    )
+      </div>
+      <TodaySection />
+      <EventCalendarSection />
+    </div>
+  );
 }
