2b16de97db3bd1526790598ff674bf2869036082
soyoung125 <sypark041754@gmail.com>
2025-01-22 12:21:07 +0900

    feat: create restaurant, shopping routes

---
+++ src/app/routes/RESTAURANT_ROUTES.tsx
@@ -0,0 +1,18 @@
+import { PATH } from "@constants/path";
+
+const RESTAURANT_ROUTES = [
+  {
+    path: PATH.restaurant,
+    element: <>restaurant</>,
+  },
+  {
+    path: PATH.restaurantSearch,
+    element: <>restaurant search</>,
+  },
+  {
+    path: PATH.restaurantInfoPath,
+    element: <>restaurant information</>,
+  },
+];
+
+export default RESTAURANT_ROUTES;
---
+++ src/app/routes/SHOPPING_ROUTES.tsx
@@ -0,0 +1,18 @@
+import { PATH } from "@constants/path";
+
+const SHOPPING_ROUTES = [
+  {
+    path: PATH.shopping,
+    element: <>shopping</>,
+  },
+  {
+    path: PATH.shoppingSearch,
+    element: <>shopping search</>,
+  },
+  {
+    path: PATH.shoppingInfoPath,
+    element: <>shopping information</>,
+  },
+];
+
+export default SHOPPING_ROUTES;
