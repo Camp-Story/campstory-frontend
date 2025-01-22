485d90b3a1db868e1d1ce6785b35e8d6de22b1e2
marie <marie061313@gmail.com>
2025-01-23 00:20:34 +0900

    design: SearchBar 스타일 수정

--- src/components/camping/campingMain/SearchBar.tsx
+++ src/components/camping/campingMain/SearchBar.tsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
-
 interface SearchBarProps {
   className: string;
-  backgroundColor?: boolean;
 }
 
-export default function SearchBar({ className, backgroundColor }: SearchBarProps) {
-  const [focus, setFocus] = useState(false);
+export default function SearchBar({ className }: SearchBarProps) {
   return (
     <div className={className}>
       <input
         placeholder="검색어를 입력하세요"
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-        className={`py-3 px-6 w-[800px] rounded-full focus:outline-green-700 ${
-          backgroundColor
-            ? focus
-              ? "bg-white placeholder-slate-600"
-              : "bg-slate-200 placeholder-slate-600"
-            : "bg-white"
-        } `}
+        className="relative py-3 px-6 w-[892px] rounded-full placeholder-gray-scale-300 focus:outline-green-700"
       />
+      {/* <img
+        src="/SearchIcon.svg"
+        alt="searchIcon"
+        className="absolute top-[50%] -translate-y-[50%] right-[30px]"
+      /> */}
     </div>
   );
 }
