d2e084e991b83d508f081911f956ee625e0df303
marie <marie061313@gmail.com>
2025-01-22 17:43:19 +0900

    fix : footer layout 수정

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -4,14 +4,14 @@ import "swiper/css";
 
 export default function RootLayout() {
   return (
-    <>
-      <div className="h-dvh font-sans w-[1440px] m-auto">
+    <div className="h-dvh">
+      <div className="font-sans w-[1440px] m-auto">
         <header className="sticky top-0 w-full bg-gray-scale-0 px-main py-3 z-20">
           <MainNavigation />
         </header>
         <Outlet />
       </div>
       <footer className="h-52 bg-gray-200 px-main py-7">footer</footer>
-    </>
+    </div>
   );
 }
