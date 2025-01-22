b027fb9ef4cf0192530faa16e01337cbc572a1c0
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 14:18:51 +0900

    fix: banner_img

--- src/components/event_components/Banner.tsx
+++ src/components/event_components/Banner.tsx
@@ -15,7 +15,7 @@ export default function Banner() {
     return (
       <>
         <div className="relative">
-          <img src="/public/festival/festival-banner.png" alt="이미지" className="w-full object-cover" />
+          <img src="/public/images/festival/festival-banner.png" alt="이미지" className="w-full object-cover" />
         </div>
       </>
     );
