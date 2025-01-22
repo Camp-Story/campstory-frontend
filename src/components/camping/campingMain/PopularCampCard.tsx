efad2781564d030b7e5533b953d8657cd6c90d55
marie <marie061313@gmail.com>
2025-01-23 01:40:31 +0900

    feat: PopularCampCard 구조 변경

--- src/components/camping/campingMain/PopularCampCard.tsx
+++ src/components/camping/campingMain/PopularCampCard.tsx
@@ -1,11 +1,21 @@
 import { useNavigate } from "react-router";
 import PopularCampCardProps from "types/PopularCampingCardProps";
 
-export default function PopularCampCard({ src, category, name, path }: PopularCampCardProps) {
+export default function PopularCampCard({ rank, src, category, name, path }: PopularCampCardProps) {
   const navigate = useNavigate();
   return (
-    <div onClick={() => navigate(path)}>
-      <img src={src} alt="CampingImage" className="w-[300px] hover:brightness-75" />
+    <div onClick={() => navigate(path)} className="flex flex-col">
+      <div className="relative h-[300px] hover:brightness-[0.8]">
+        <img src={src} alt="CampingImage" className="absolute top-0 w-[300px]" />
+        <img
+          src="images/camping/Gradient.png"
+          alt="gradient"
+          className="absolute top-0 w-[300px]"
+        />
+        <div className="absolute font-impact text-gray-scale-0 text-[100px] top-[188px] left-10">
+          {rank}
+        </div>
+      </div>
       <div className="text-body1 font-bold mt-4 mb-[4px] text-gray-scale-300">{category}</div>
       <div className="text-sub-title font-extrabold text-gray-scale-400">{name}</div>
     </div>
