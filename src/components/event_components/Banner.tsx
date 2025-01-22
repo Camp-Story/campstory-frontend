29de2ac9e878e22b68e950dcb4fa463b92cecf48
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 18:26:10 +0900

    add: searchbar_component

--- src/components/event_components/Banner.tsx
+++ src/components/event_components/Banner.tsx
@@ -1,10 +1,10 @@
+import SearchBar from "./SearchBar";
 
 export default function Banner() {
-    return (
-      <>
-        <div>
-          <img src="/images/festival/festival-banner.png" alt="이미지" className="w-full object-cover mb-16" />
-        </div>
-      </>
-    );
-  }
+  return (
+    <div className="relative">
+      <img src="/images/festival/festival-banner.png" alt="festival banner" className="w-full" />
+      <SearchBar className="absolute bottom-[60px] left-[50%] -translate-x-[50%]" />
+    </div>
+  );
+}
