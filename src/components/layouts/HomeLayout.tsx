fdafe39e51d792817f3a8eca2fc9945ce37f3251
Sean <joajoa70584@gmail.com>
2025-01-22 12:29:43 +0900

    feat: 레이아웃 컴포넌트 헤더 스타일 변경

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
+import MainNavigation from "./MainNavigation";
 import "swiper/css";
 
 export default function HomeLayout() {
   return (
     <div className="h-dvh font-sans">
-      <div className="fixed top-0 w-full bg-green-200">header</div>
-      <div className="my-10">
+      <div className="sticky top-0 w-full bg-gray-scale-0 px-main py-[10px]">
+        <MainNavigation />
+      </div>
+      <div className="">
         <Outlet />
         redo the test1
       </div>
