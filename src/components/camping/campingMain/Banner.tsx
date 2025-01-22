7ab4447601dd58cf5d832791281c12412170d9ba
marie <marie061313@gmail.com>
2025-01-22 14:36:17 +0900

    feat: SearchBar 컴포넌트 구현

--- src/components/camping/campingMain/Banner.tsx
+++ src/components/camping/campingMain/Banner.tsx
@@ -1,7 +1,10 @@
+import SearchBar from "./SearchBar";
+
 export default function Banner() {
   return (
     <div className="relative">
       <img src="/images/camping/camping-banner.png" alt="camping banner" className="w-full" />
+      <SearchBar className="absolute bottom-[60px] left-[50%] -translate-x-[50%]" />
     </div>
   );
 }
