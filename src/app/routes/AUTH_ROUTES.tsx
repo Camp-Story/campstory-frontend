a7a9aa39e248f0bdf0dd63bd1a4466f19bf84353
soyoung125 <sypark041754@gmail.com>
2025-01-22 12:19:57 +0900

    feat: create auth routes

---
+++ src/app/routes/AUTH_ROUTES.tsx
@@ -0,0 +1,34 @@
+import { PATH } from "@constants/path";
+
+const AUTH_ROUTES = [
+  {
+    path: PATH.signUp,
+    element: <>signUp</>,
+  },
+  {
+    path: PATH.login,
+    element: <>login</>,
+  },
+  {
+    path: PATH.mypage,
+    element: <>mypage</>,
+  },
+  {
+    path: PATH.information,
+    element: <>mypage information</>,
+  },
+  {
+    path: PATH.reservedList,
+    element: <>mypage reservedList</>,
+  },
+  {
+    path: PATH.bookmark,
+    element: <>mypage bookmark</>,
+  },
+  {
+    path: PATH.activities,
+    element: <>mypage activities</>,
+  },
+];
+
+export default AUTH_ROUTES;
