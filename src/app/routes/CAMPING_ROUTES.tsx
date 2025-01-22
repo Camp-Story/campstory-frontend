f10537249f00539fc4dd76419efe8efc10d7490a
soyoung125 <sypark041754@gmail.com>
2025-01-22 12:20:09 +0900

    feat: create comping routes

---
+++ src/app/routes/CAMPING_ROUTES.tsx
@@ -0,0 +1,22 @@
+import { PATH } from "@constants/path";
+
+const CAMPING_ROUTES = [
+  {
+    path: PATH.camping,
+    element: <>camping</>,
+  },
+  {
+    path: PATH.campingSearch,
+    element: <>camping search</>,
+  },
+  {
+    path: PATH.campingInfoPath,
+    element: <>camping information</>,
+  },
+  {
+    path: PATH.campingReservationPath,
+    element: <>camping reservation</>,
+  },
+];
+
+export default CAMPING_ROUTES;
