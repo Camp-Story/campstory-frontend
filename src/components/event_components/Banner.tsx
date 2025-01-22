3e81bf593f06f3233949497435180db56758602b
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 14:30:57 +0900

    fix: banner mb-16

--- src/components/event_components/Banner.tsx
+++ src/components/event_components/Banner.tsx
@@ -3,7 +3,7 @@ export default function Banner() {
     return (
       <>
         <div className="relative">
-          <img src="/public/images/festival/festival-banner.png" alt="이미지" className="w-full object-cover" />
+          <img src="/public/images/festival/festival-banner.png" alt="이미지" className="w-full object-cover mb-16" />
         </div>
       </>
     );
