4d0bb0ce0a58369418e2c5fb712ca29cc9d20228
Sean <joajoa70584@gmail.com>
2025-01-22 12:41:20 +0900

    feat: 시멘틱 마크업 및 푸터 마무리

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -5,14 +5,14 @@ import "swiper/css";
 export default function HomeLayout() {
   return (
     <div className="h-dvh font-sans">
-      <div className="sticky top-0 w-full bg-gray-scale-0 px-main py-[10px]">
+      <header className="sticky top-0 w-full bg-gray-scale-0 px-main py-[10px]">
         <MainNavigation />
-      </div>
-      <div className="">
+      </header>
+      <main className="px-main">
         <Outlet />
         redo the test1
-      </div>
-      <div className="font-impact text-highlight">footer</div>
+      </main>
+      <footer className="h-52 bg-gray-200 px-main py-7">footer</footer>
     </div>
   );
 }
