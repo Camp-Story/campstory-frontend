857dcd5b0f849906c66da2cff03e664599b9a46e
soyoung125 <sypark041754@gmail.com>
2025-01-22 17:02:14 +0900

    feat: add camping search filter

---
+++ src/constants/filters.ts
@@ -0,0 +1,76 @@
+export const CAMPING_CATEGORY = [
+  {
+    value: "regular-campsite",
+    label: "일반 야영장",
+  },
+  {
+    value: "glamping",
+    label: "글램핑",
+  },
+  {
+    value: "caravan",
+    label: "카라반",
+  },
+  {
+    value: "car-campsite",
+    label: "차박",
+  },
+];
+
+export const AREA = [
+  {
+    code: 11,
+    label: "서울특별시",
+    shorten: "서울",
+  },
+  {
+    code: 50,
+    label: "제주특별자치도",
+    shorten: "제주",
+  },
+  {
+    code: 51,
+    label: "강원특별자치도",
+    shorten: "강원도",
+  },
+  {
+    code: 26,
+    label: "부산광역시",
+    shorten: "부산",
+  },
+  {
+    code: 46,
+    label: "전라남도",
+    shorten: "전남",
+  },
+  {
+    code: 52,
+    label: "전북특별자치도",
+    shorten: "전북",
+  },
+  {
+    code: 43,
+    label: "충청북도",
+    shorten: "충북",
+  },
+  {
+    code: 44,
+    label: "충청남도",
+    shorten: "충남",
+  },
+  {
+    code: 41,
+    label: "경기도",
+    shorten: "경기도",
+  },
+  {
+    code: 47,
+    label: "경상북도",
+    shorten: "경북",
+  },
+  {
+    code: 48,
+    label: "경상남도",
+    shorten: "경남",
+  },
+];
