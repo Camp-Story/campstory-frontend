c820de49942a354d44fb52bf17dba26faeaae600
Sean <joajoa70584@gmail.com>
2025-01-22 16:11:51 +0900

    feat: 홈 페이지 인기 캠핑장 섹션 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,34 @@
+import CampingSpotItemType from "../types/CampingSpotItemType";
+import CampingSpotItem from "@components/home/CampingSpotItem";
 import CampingSearchForm from "../components/home/CampingSearchForm";
+
+const DUMMY: CampingSpotItemType[] = [
+  {
+    src: "",
+    name: "속초 밤하늘 글램핑",
+    category: "글램핑•펜션•오토캠핑",
+  },
+  {
+    src: "",
+    name: "속초 밤하늘 글램핑",
+    category: "글램핑•펜션•오토캠핑",
+  },
+  {
+    src: "",
+    name: "속초 밤하늘 글램핑",
+    category: "글램핑•펜션•오토캠핑",
+  },
+  {
+    src: "",
+    name: "속초 밤하늘 글램핑",
+    category: "글램핑•펜션•오토캠핑",
+  },
+  {
+    src: "",
+    name: "속초 밤하늘 글램핑",
+    category: "글램핑•펜션•오토캠핑",
+  },
+];
 
 function Home() {
   return (
@@ -10,6 +40,22 @@ function Home() {
           className="w-full object-cover"
         />
         <CampingSearchForm />
+      </section>
+      <section className="mb-14">
+        <h2 className="text-highlight font-impact mb-2">인기 캠핑장</h2>
+        <p className="text-sub-title text-gray-scale-300 mb-7">
+          나를 위한 근사한 휴가를 계획해보세요.
+        </p>
+        <ul className="flex gap-8">
+          {DUMMY.map((item) => (
+            <CampingSpotItem
+              key={item.src}
+              src={item.src}
+              name={item.name}
+              category={item.category}
+            />
+          ))}
+        </ul>
       </section>
     </>
   );
