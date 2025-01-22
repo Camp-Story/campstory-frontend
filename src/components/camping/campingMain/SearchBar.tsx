b5b8afe484648e93bc405ac7828e3c3fb4ed3a04
marie <marie061313@gmail.com>
2025-01-23 01:53:54 +0900

    feat: SearchBar 컴포넌트 버튼 추가

--- src/components/camping/campingMain/SearchBar.tsx
+++ src/components/camping/campingMain/SearchBar.tsx
@@ -1,19 +1,24 @@
+import { PATH } from "@constants/path";
+import { useNavigate } from "react-router";
+
 interface SearchBarProps {
   className: string;
 }
 
 export default function SearchBar({ className }: SearchBarProps) {
+  const navigate = useNavigate();
   return (
     <div className={className}>
       <input
-        placeholder="검색어를 입력하세요"
+        placeholder="검색어를 입력해주세요."
         className="relative py-3 px-6 w-[892px] rounded-full placeholder-gray-scale-300 focus:outline-green-700"
       />
-      {/* <img
-        src="/SearchIcon.svg"
-        alt="searchIcon"
-        className="absolute top-[50%] -translate-y-[50%] right-[30px]"
-      /> */}
+      <button
+        onClick={() => navigate(PATH.campingSearch)}
+        className="absolute top-[50%] -translate-y-[50%] right-[20px] p-2"
+      >
+        <img src="/SearchIcon.svg" alt="searchIcon" />
+      </button>
     </div>
   );
 }
