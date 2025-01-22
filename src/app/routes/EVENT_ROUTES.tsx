d4cfabb5d08b9c46feb11654cfed4ee6dfdf813e
soyoung125 <sypark041754@gmail.com>
2025-01-22 12:20:40 +0900

    feat: create event routes

---
+++ src/app/routes/EVENT_ROUTES.tsx
@@ -0,0 +1,18 @@
+import { PATH } from "@constants/path";
+
+const EVENT_ROUTES = [
+  {
+    path: PATH.event,
+    element: <>event</>,
+  },
+  {
+    path: PATH.eventSearch,
+    element: <>event search</>,
+  },
+  {
+    path: PATH.eventInfoPath,
+    element: <>event information</>,
+  },
+];
+
+export default EVENT_ROUTES;
