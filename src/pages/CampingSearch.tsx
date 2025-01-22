4fc9760dfe1ca69d4fb22003ec3147c9712d397c
soyoung125 <sypark041754@gmail.com>
2025-01-22 22:05:20 +0900

    feat: create search page card component

--- src/pages/CampingSearch.tsx
+++ src/pages/CampingSearch.tsx
@@ -1,4 +1,6 @@
+import SearchCard from "@components/common/search/SearchCard";
 import SearchInput from "@components/common/SearchInput";
+
 import { AREA, CAMPING_CATEGORY } from "@constants/filters";
 
 export default function CampingSearch() {
@@ -7,7 +9,8 @@ export default function CampingSearch() {
       <div className="mt-[100px] mb-[60px]">
         <SearchInput handleSubmit={() => alert("submit!")} />
       </div>
-      <div className="flex gap-[34px]">
+
+      <div className="flex gap-[34px] pb-5">
         <div className="flex flex-col gap-[30px]">
           <div className="relative">
             <img src="/images/map-small.png" alt="map small" />
@@ -25,7 +28,7 @@ export default function CampingSearch() {
             <h4 className="text-base font-bold">카테고리</h4>
             <ul className="w-48 flex flex-col gap-5 text-[15px] font-medium text-gray-scale-400">
               {CAMPING_CATEGORY.map((category) => (
-                <li className="w-full border-none">
+                <li className="w-full border-none" key={category.value}>
                   <div className="flex gap-2 items-center">
                     <input
                       id={category.value}
@@ -46,7 +49,7 @@ export default function CampingSearch() {
             <h4 className="text-base font-bold">지역</h4>
             <div className="grid grid-cols-3 gap-2.5 w-[230px]">
               {AREA.map((area) => (
-                <div className="flex gap-2 items-center w-fit">
+                <div className="flex gap-2 items-center w-fit" key={area.code}>
                   <input id={area.label} type="checkbox" value="" className="hidden peer" />
                   <label
                     htmlFor={area.label}
@@ -57,6 +60,16 @@ export default function CampingSearch() {
                 </div>
               ))}
             </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-[30px]">
+          <h2 className="text-[26px] font-bold text-gray-scale-400">'캠핑' 검색 결과 14개</h2>
+          <div className="grid grid-cols-2 gap-x-5 gap-y-[30px]">
+            <SearchCard />
+            <SearchCard />
+            <SearchCard />
+            <SearchCard />
           </div>
         </div>
       </div>
