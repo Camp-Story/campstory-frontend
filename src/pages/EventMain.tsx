0da9074ffba66dcae2644fd9a444786e70e03e5d
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 14:31:21 +0900

    add: subtitle

--- src/pages/EventMain.tsx
+++ src/pages/EventMain.tsx
@@ -1,10 +1,12 @@
 import Banner from "@components/event_components/Banner"
+import SubTitle from "@components/event_components/SubTitle"
 
 export default function EventMain() {
 
     return (
         <div>
             <Banner/>
+            <SubTitle mainText="다양한 축제, 새로운 즐거움" subText="음식, 음악, 자연, 문화가 어우러진 축제의 세계를 만나보세요"/>
             <h1>안녕하세요!</h1>
             <h2>반갑습니다!</h2>
         </div>
