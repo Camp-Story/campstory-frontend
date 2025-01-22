d791fa1149706f1f2e4d40452f3e4295b9cb19b5
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 12:19:20 +0900

    chage: banner_components_text

--- src/components/event_components/Banner.tsx
+++ src/components/event_components/Banner.tsx
@@ -16,10 +16,6 @@ export default function Banner() {
       <>
         <div className="relative">
           <img src="/public/festival/festival-banner.png" alt="이미지" className="w-full object-cover" />
-          <div className=" w-[800px] mx-auto absolute bottom-[140px] left-[50%] -translate-x-[50%] text-[32px] text-white">
-            <div className="-my-1">자연속 특별한 순간을 만나는 캠핑,</div>
-            <div className=" font-extrabold">지금 바로 떠날 준비 되셨나요?</div>
-          </div>
         </div>
       </>
     );
