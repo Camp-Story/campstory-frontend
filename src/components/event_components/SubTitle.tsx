18b6b774ac0eb3e74daabc8560c2152a0828be16
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 14:13:26 +0900

    add: SubTitie_component

---
+++ src/components/event_components/SubTitle.tsx
@@ -0,0 +1,17 @@
+interface SubTitleProps {
+    mainText: string;
+    subText: string;
+}
+
+export default function SubTitle({ mainText, subText }: SubTitleProps) {
+    return (
+        <div className="mb-4">
+            <div className="font-extrabold text-[36px] text-[#333333]">
+                {mainText}
+            </div>
+            <div className="text-[20px] text-[#676767]">
+                {subText}
+            </div>
+        </div>
+    );
+}
