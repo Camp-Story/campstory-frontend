0318ad142498abda4dc79bcfff79dad65b9aa73c
marie <marie061313@gmail.com>
2025-01-22 22:21:29 +0900

    design: CategoryCard 컴포넌트 hover 스타일 추가

--- src/components/camping/campingMain/CategoryCard.tsx
+++ src/components/camping/campingMain/CategoryCard.tsx
@@ -8,7 +8,7 @@ interface CategoryCardProps {
 export default function CategoryCard({ src, children }: CategoryCardProps) {
   return (
     <div>
-      <img src={src} alt="Camping Image" />
+      <img src={src} alt="Camping Image" className="hover:brightness-75" />
       <div className="text-[26px] text-gray-scale-400 font-bold my-4">{children}</div>
     </div>
   );
