435770a7ce03a3bb77d03ed28c8892d641683647
marie <marie061313@gmail.com>
2025-01-22 16:51:21 +0900

    refactor: PopularCampCardProps 인터페이스 수정

--- src/components/camping/campingMain/PopularCampCard.tsx
+++ src/components/camping/campingMain/PopularCampCard.tsx
@@ -1,11 +1,11 @@
 import PopularCampCardProps from "types/PopularCampingCardProps";
 
-export default function PopularCampCard({ src, category, children }: PopularCampCardProps) {
+export default function PopularCampCard({ src, category, name }: PopularCampCardProps) {
   return (
     <div>
       <img src={src} alt="CampingImage" className="w-[300px]" />
       <div className="text-body1 font-bold mt-4 mb-[4px] text-gray-scale-300">{category}</div>
-      <div className="text-sub-title font-extrabold text-gray-scale-400">{children}</div>
+      <div className="text-sub-title font-extrabold text-gray-scale-400">{name}</div>
     </div>
   );
 }
