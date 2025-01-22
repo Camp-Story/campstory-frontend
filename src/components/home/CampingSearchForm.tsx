ba9d20a28e1ae7d9b00cfce6bef9c6440cde5bac
Sean <joajoa70584@gmail.com>
2025-01-22 15:10:36 +0900

    feat: 홈 화면 검색 폼 컴포넌트 추가

---
+++ src/components/home/CampingSearchForm.tsx
@@ -0,0 +1,42 @@
+export default function CampingSearchForm() {
+  return (
+    <div className="absolute -bottom-10 left-1/2 transform -translate-x-1/2 w-3/5 py-8 px-12 bg-white rounded-xl drop-shadow-custom flex items-end gap-4">
+      <div className="flex flex-col gap-4 w-60">
+        <label htmlFor="camping" className="text-sub-title font-bold text-gray-scale-400">
+          캠핑장
+        </label>
+        <input
+          type="text"
+          id="camping"
+          placeholder="캠핑장 검색"
+          className="w-full px-5 py-4 rounded text-body1 bg-gray-scale-100 focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-4 w-52">
+        <label htmlFor="date" className="text-sub-title font-bold text-gray-scale-400">
+          날짜
+        </label>
+        <input
+          type="text"
+          id="date"
+          placeholder="01.16 목 - 01-17 금 (1박)"
+          className="w-full px-5 py-4 rounded text-body1 bg-gray-scale-100 focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-4 w-52">
+        <label htmlFor="people" className="text-sub-title font-bold text-gray-scale-400">
+          인원
+        </label>
+        <input
+          type="number"
+          id="people"
+          placeholder="인원 2"
+          className="w-full px-5 py-4 rounded text-body1 bg-gray-scale-100 focus:outline-none"
+        />
+      </div>
+      <button className="flex-1 py-4 bg-secondary-300 text-body1 text-gray-scale-0 rounded hover:bg-secondary-500 transition">
+        검색
+      </button>
+    </div>
+  );
+}
