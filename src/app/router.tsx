3356b47dd04e37384605c68f869bb991dc59c377
soyoung125 <sypark041754@gmail.com>
2025-01-22 12:21:21 +0900

    feat: apply all routes to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,18 +1,44 @@
 import HomeLayout from "@components/layouts/HomeLayout";
 import { createBrowserRouter } from "react-router";
 import HOME_ROUTES from "./routes/HOME_ROUTES";
+import RootLayout from "@components/layouts/RootLayout";
+import CAMPING_ROUTES from "./routes/CAMPING_ROUTES";
+import EVENT_ROUTES from "./routes/EVENT_ROUTES";
+import SHOPPING_ROUTES from "./routes/SHOPPING_ROUTES";
+import RESTAURANT_ROUTES from "./routes/RESTAURANT_ROUTES";
+import COMMUNITY_ROUTES from "./routes/COMMUNITY_ROUTES";
+import QUESTION_ROUTES from "./routes/QUESTION_ROUTES";
+import AUTH_ROUTES from "./routes/AUTH_ROUTES";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomeLayout />,
+    element: <RootLayout />,
     children: [
+      {
+        path: "",
+        element: <HomeLayout />,
+        children: [
+          {
+            path: "/",
+            children: [
+              ...HOME_ROUTES,
+              ...CAMPING_ROUTES,
+              ...EVENT_ROUTES,
+              ...SHOPPING_ROUTES,
+              ...RESTAURANT_ROUTES,
+              ...COMMUNITY_ROUTES,
+              ...QUESTION_ROUTES,
+            ],
+          },
+        ],
+      },
       {
         path: "/",
         children: [
           {
             path: "/",
-            children: HOME_ROUTES,
+            children: AUTH_ROUTES,
           },
         ],
       },
