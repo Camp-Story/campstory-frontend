19a191ad946d257033cdc88f26acbb5f097561d4
Sean <joajoa70584@gmail.com>
2025-01-22 23:37:14 +0900

    feat: 인기 캠핑장 섹션 스와이퍼 적용 및 일부 수정

--- src/components/home/CampingSpotItem.tsx
+++ src/components/home/CampingSpotItem.tsx
@@ -1,15 +1,15 @@
+import { Link } from "react-router";
 import CampingSpotItemType from "../../types/CampingSpotItemType";
 
-export default function CampingSpotItem({ src, name, category }: CampingSpotItemType) {
+export default function CampingSpotItem({ path, src, name, category }: CampingSpotItemType) {
   return (
     <li>
-      <div className="w-56 h-56 rounded-xl bg-gray-scale-100 mb-4 overflow-hidden">
-        <img
-          src={src || "/images/camping/camping-category-1.png"}
-          alt="캠핑장 이미지"
-          className="w-full h-full object-cover"
-        />
-      </div>
+      <Link
+        to={path || "/"}
+        className="block w-56 h-56 rounded-xl bg-gray-scale-100 mb-4 overflow-hidden"
+      >
+        <img src={src || ""} alt="캠핑장 이미지" className="w-full h-full object-cover" />
+      </Link>
       <h3 className="text-sub-title font-bold mb-1">{name}</h3>
       <p className="text-gray-scale-300">{category}</p>
     </li>
