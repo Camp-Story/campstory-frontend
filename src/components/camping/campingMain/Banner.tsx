d91999433c63c4f9cf2538b54e532b8ee74e4b7b
marie <marie061313@gmail.com>
2025-01-22 12:44:20 +0900

    feat: Banner 컴포넌트 구현

---
+++ src/components/camping/campingMain/Banner.tsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+  return (
+    <div className="relative">
+      <img src="/images/camping/camping-banner.png" alt="camping banner" className="w-full" />
+    </div>
+  );
+}
