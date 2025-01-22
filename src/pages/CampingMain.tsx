99b593603b7a369b015232fef57f64183938ec5e
marie <marie061313@gmail.com>
2025-01-22 14:12:41 +0900

    feat: Subtitle 컴포넌트 구현

--- src/pages/CampingMain.tsx
+++ src/pages/CampingMain.tsx
@@ -1,9 +1,13 @@
 import Banner from "@components/camping/campingMain/Banner";
+import Subtitle from "@components/camping/campingMain/Subtitle";
 
 export default function CampingMain() {
   return (
     <div>
       <Banner />
+      <div>
+        <Subtitle>카테고리별 캠핑장</Subtitle>
+      </div>
     </div>
   );
 }
