492052fe7e4606a3457ae8e2cef183db4c7abf6a
marie <marie061313@gmail.com>
2025-01-22 15:34:16 +0900

    feat: ReviewCard 컴포넌트 구현

---
+++ src/components/camping/campingMain/ReviewCard.tsx
@@ -0,0 +1,26 @@
+interface ReviewCardProps {
+  src: string;
+  contents: string;
+  timestamp: string;
+  userId: string;
+}
+
+export default function ReviewCard({ src, contents, timestamp, userId }: ReviewCardProps) {
+  return (
+    <div className="flex gap-2">
+      <img src={src} alt="reviewImg" className="w-[200px] h-[200px]" />
+      <div className="flex flex-col gap-2 border-2 border-slate-100 rounded-xl w-[400px] h-[200px] p-6">
+        <div className="text-gray-scale-400 text-body1 leading-5">{contents}</div>
+        <div className="text-gray-scale-300 text-body1">{timestamp}</div>
+        <div className="flex flex-row gap-2 items-center mt-5">
+          <img
+            src="https://picsum.photos/200"
+            alt="user"
+            className="w-[34px] h-[34px] rounded-full "
+          />
+          <div className="text-gray-scale-400 text-body1 font-bold">{userId}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
