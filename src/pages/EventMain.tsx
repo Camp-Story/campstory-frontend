a78d1d4ac478554d4e6a93f86295a000334fab01
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 12:15:10 +0900

    add: EventMainPage banner add

--- src/pages/EventMain.tsx
+++ src/pages/EventMain.tsx
@@ -1,7 +1,10 @@
+import Banner from "@components/event_components/Banner"
+
 export default function EventMain() {
 
     return (
         <div>
+            <Banner/>
             <h1>안녕하세요!</h1>
             <h2>반갑습니다!</h2>
         </div>
