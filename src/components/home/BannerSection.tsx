80d61d7c5cbbf5785b48bdc398976655f02cb3a1
Sean <joajoa70584@gmail.com>
2025-01-22 23:35:48 +0900

    feat: 배너 검색바 컴포넌트 이름 변경

---
+++ src/components/home/BannerSection.tsx
@@ -0,0 +1,14 @@
+import SearchForm from "./SearchForm";
+
+export default function BannerSection() {
+  return (
+    <section className="relative mb-24">
+      <img
+        src="/images/main/main-banner-2.png"
+        alt="자연 속에서 써내려가는 우리의 이야기, CAMP STORY"
+        className="w-full object-cover"
+      />
+      <SearchForm />
+    </section>
+  );
+}
