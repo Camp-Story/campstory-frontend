4bf1c4765bf455944d719b173f6ef3588e637d45
leejihwan99 <lbwlimes@gmail.com>
2025-01-22 12:10:55 +0900

    add: event_banner_componets add

--- src/components/event_components/Banner.tsx
+++ src/components/event_components/Banner.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import SearchBar from "./SearchBar";
 
 export default function Banner() {
@@ -8,3 +9,19 @@ export default function Banner() {
     </div>
   );
 }
+=======
+
+export default function Banner() {
+    return (
+      <>
+        <div className="relative">
+          <img src="/public/festival/festival-banner.png" alt="이미지" className="w-full object-cover" />
+          <div className=" w-[800px] mx-auto absolute bottom-[140px] left-[50%] -translate-x-[50%] text-[32px] text-white">
+            <div className="-my-1">자연속 특별한 순간을 만나는 캠핑,</div>
+            <div className=" font-extrabold">지금 바로 떠날 준비 되셨나요?</div>
+          </div>
+        </div>
+      </>
+    );
+  }
+>>>>>>> 6650190 (add: event_banner_componets add)
