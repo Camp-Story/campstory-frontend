bc89142a75b3ce2cb1bf5486e9158a3bd743b057
soyoung125 <sypark041754@gmail.com>
2025-01-22 14:09:00 +0900

    refactor: update layout

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router";
-import MainNavigation from "./MainNavigation";
 import "swiper/css";
 
 export default function HomeLayout() {
   return (
-    <div className="h-dvh font-sans">
-      <header className="sticky top-0 w-full bg-gray-scale-0 px-main py-[10px]">
-        <MainNavigation />
-      </header>
+    <div>
       <main className="px-main">
         <Outlet />
         redo the test1
