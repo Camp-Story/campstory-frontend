8e7d02d7be696db7ad1f7d2d46cd3ab82b8579c2
soyoung125 <sypark041754@gmail.com>
2025-01-23 14:56:27 +0900

    design: chagne checkbox cursor

--- src/components/common/search/CategoryCheckbox.tsx
+++ src/components/common/search/CategoryCheckbox.tsx
@@ -5,15 +5,15 @@ interface CategoryCheckboxProps {
 
 export default function CategoryCheckbox({ label, value }: CategoryCheckboxProps) {
   return (
-    <li className="w-full border-none">
+    <li className="w-full border-none cursor-pointer">
       <div className="flex gap-2 items-center">
         <input
           id={value}
           type="checkbox"
           value=""
-          className="appearance-none w-6 h-5 rounded-full checked:bg-info-500 checked:border-info-500 bg-gray-scale-100 border-gray-scale-300"
+          className="cursor-pointer appearance-none w-6 h-5 rounded-full checked:bg-info-500 checked:border-info-500 bg-gray-scale-100 border-gray-scale-300"
         />
-        <label htmlFor={value} className="w-full">
+        <label htmlFor={value} className="w-full cursor-pointer">
           {label}
         </label>
       </div>
