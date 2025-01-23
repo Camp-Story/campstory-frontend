1e23a75c347898e26bc6999f4f502a96460051b6
soyoung125 <sypark041754@gmail.com>
2025-01-24 00:36:48 +0900

    refactor: add click event to postcard

--- src/components/community/Badge.tsx
+++ src/components/community/Badge.tsx
@@ -1,11 +1,27 @@
+import { twMerge } from "tailwind-merge";
+
 interface BadgeProps {
   icon: JSX.Element;
   count?: number;
+  handleClick?: () => void;
 }
 
-export default function Badge({ count, icon }: BadgeProps) {
+export default function Badge({ count, icon, handleClick }: BadgeProps) {
+  const handleClickBadge = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (handleClick) {
+      e.stopPropagation();
+      handleClick();
+    }
+  };
+
   return (
-    <div className="flex gap-1 items-center border border-gray-scale-200 rounded-xl text-gray-scale-200 text-[13px] px-2.5 py-1 bg-white hover:brightness-95">
+    <div
+      className={twMerge(
+        "flex gap-1 items-center border border-gray-scale-200 rounded-xl text-gray-scale-200 text-[13px] px-2.5 py-1 bg-white",
+        handleClick ? "hover:brightness-95" : "",
+      )}
+      onClick={handleClickBadge}
+    >
       {icon}
       {count && <div>{count}</div>}
     </div>
