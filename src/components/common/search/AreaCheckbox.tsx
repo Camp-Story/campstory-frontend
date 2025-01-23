d3af69a89aec4a665d6a7a4f4623179bef4deaf3
soyoung125 <sypark041754@gmail.com>
2025-01-23 14:52:35 +0900

    design: apply hover to area checkbox

--- src/components/common/search/AreaCheckbox.tsx
+++ src/components/common/search/AreaCheckbox.tsx
@@ -10,7 +10,7 @@ export default function AreaCheckbox({ code, label, value }: AreaCheckboxProps) 
       <input id={value} type="checkbox" value="" className="hidden peer" />
       <label
         htmlFor={value}
-        className="border border-gray-scale-100 peer-checked:border-[#1A9EFE]/50 peer-checked:bg-[#1A9EFE]/15 peer-checked:text-info-500 w-[70px] text-center py-1 rounded-[20px] text-[13px] font-bold"
+        className="border border-gray-scale-100 hover:bg-gray-scale-100/50 peer-checked:border-[#1A9EFE]/50 peer-checked:bg-[#1A9EFE]/15 peer-checked:hover:bg-[#1A9EFE]/30 peer-checked:text-info-500 w-[70px] text-center py-1 rounded-[20px] text-[13px] font-bold"
       >
         {label}
       </label>
