9de32e3d5f517bbf820335bb1e67511166e13539
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2025-01-23 18:23:17 +0900

    feat:카테고리,필터,페이지

--- src/components/shopping/Category.tsx
+++ src/components/shopping/Category.tsx
@@ -7,10 +7,14 @@ interface CategoryProps {
 export default function Category({ path, src, catName }: CategoryProps) {
   return (
     <div onClick={() => alert(path)}>
-      <div className="flex flex-col items-center justify-center rounded-full bg-gray-scale-100 w-[63px] h-[63px]">
-        <img src={src} alt="" className=" w-[40px] h-[40px]" />
+      <div className="flex flex-col items-center justify-center text-center rounded-full bg-gray-scale-100 w-[63px] h-[63px]">
+        <img
+          src={src}
+          alt=""
+          className="flex flex-col items-center justify-center text-center w-[40px] h-[40px]"
+        />
       </div>
-      <span className="text-[15px] text-center">{catName}</span>
+      <p className="text-[15px] mt-[15px] text-center">{catName}</p>
     </div>
   );
 }
---
+++ src/components/shopping/ProductCardList.tsx
@@ -0,0 +1,226 @@
+import ProductCard from "./ProductCard";
+
+export default function ProductCardList() {
+  const products = [
+    {
+      id: 1,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item01.png",
+    },
+    {
+      id: 2,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item02.png",
+    },
+    {
+      id: 3,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item03.png",
+    },
+    {
+      id: 4,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item04.png",
+    },
+    {
+      id: 5,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item05.png",
+    },
+    {
+      id: 6,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item06.png",
+    },
+    {
+      id: 7,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item07.png",
+    },
+    {
+      id: 8,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item08.png",
+    },
+    {
+      id: 9,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item09.png",
+    },
+    {
+      id: 10,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item10.png",
+    },
+    {
+      id: 1,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item01.png",
+    },
+    {
+      id: 2,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item02.png",
+    },
+    {
+      id: 3,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item03.png",
+    },
+    {
+      id: 4,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item04.png",
+    },
+    {
+      id: 5,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item05.png",
+    },
+    {
+      id: 6,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item06.png",
+    },
+    {
+      id: 7,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item07.png",
+    },
+    {
+      id: 8,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item08.png",
+    },
+    {
+      id: 9,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item09.png",
+    },
+    {
+      id: 10,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item10.png",
+    },
+    {
+      id: 6,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item06.png",
+    },
+    {
+      id: 7,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item07.png",
+    },
+    {
+      id: 8,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item08.png",
+    },
+    {
+      id: 9,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item09.png",
+    },
+    {
+      id: 10,
+      brand: "브랜드 이름",
+      name: "시어플러스57 배틀큐 그릴",
+      discount: "30%",
+      price: "168,000원",
+      img: "/images/shopping/shoppingItems/item10.png",
+    },
+  ];
+
+  return (
+    <div className="mt-[50px] grid grid-cols-5 gap-4">
+      {products.map((product, idx) => (
+        <div className="mt-[40px]">
+          <ProductCard
+            img={product.img}
+            bookmarked={false}
+            key={idx}
+            brandName={product.brand}
+            discount={product.discount}
+            productName={product.name}
+            price={product.price}
+            handleClick={() => alert("click")}
+            handleClickBookmark={() => alert("bookmark")}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
