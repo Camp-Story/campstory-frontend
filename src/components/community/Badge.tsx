259d2da9a7409f4f5a4b5733facbda84a9c89c92
soyoung125 <sypark041754@gmail.com>
2025-01-24 00:25:40 +0900

    design: apply brightness to badge

--- src/components/community/Badge.tsx
+++ src/components/community/Badge.tsx
@@ -5,7 +5,7 @@ interface BadgeProps {
 
 export default function Badge({ count, icon }: BadgeProps) {
   return (
-    <div className="flex gap-1 items-center border border-gray-scale-200 rounded-xl text-gray-scale-200 text-[13px] px-2.5 py-1">
+    <div className="flex gap-1 items-center border border-gray-scale-200 rounded-xl text-gray-scale-200 text-[13px] px-2.5 py-1 bg-white hover:brightness-95">
       {icon}
       {count && <div>{count}</div>}
     </div>
