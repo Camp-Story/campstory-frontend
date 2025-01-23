d13daf193f160fdedaebf476c3af4f2b7582081c
leejihwan99 <lbwlimes@gmail.com>
2025-01-23 15:24:32 +0900

    feat: 인근명소 컴포넌트 추가

---
+++ src/components/camping/campingDetail/NearbyPlacesSection.tsx
@@ -0,0 +1,152 @@
+export default function NearbyPlacesSection() {
+  return (
+    <section>
+      <div className="flex flex-wrap justify-between items-center mb-5">
+        <div className="text-[26px] font-bold">인근 명소</div>
+        <div className="text-[20px] font-bold text-info-500">더보기</div>
+      </div>
+      <div className="flex flex-wrap justify-between">
+        <div>
+          <img
+            src="/images/camping/camping-popular-dummy1.png"
+            alt="인근 명소 이미지"
+            className="w-[228px] h-[228px] rounded-xl mb-[10px]"
+          />
+          <div className="flex justify-between items-center">
+            <div>
+              <div className="text-[13px] font-bold text-gray-scale-300 mb-[7px]">카테고리</div>
+              <div className="text-[15px]">주변 맛집 이름</div>
+            </div>
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="15" cy="15" r="15" fill="#D9D9D9" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M14.9999 8.38854C15.8749 7.49946 17.0144 7 18.2039 7C19.4884 7 20.7146 7.58238 21.6124 8.60881C22.5038 9.62713 23 10.9998 23 12.4254C23 13.851 22.5037 15.2238 21.6124 16.242C21.0197 16.9194 20.4279 17.6122 19.8331 18.3086C18.6249 19.723 17.4038 21.1526 16.1353 22.4995L16.1324 22.5026C15.4782 23.187 14.4422 23.1621 13.816 22.4465L8.38716 16.2419C6.53761 14.1281 6.53761 10.7227 8.38716 8.6089C10.1971 6.54038 13.1115 6.46693 14.9999 8.38854Z"
+                fill="#B4B4B4"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/public/images/camping/camping-popular-dummy1.png"
+            alt="인근 명소 이미지"
+            className="w-[228px] h-[228px] rounded-xl mb-[10px]"
+          />
+          <div className="flex justify-between items-center">
+            <div>
+              <div className="text-[13px] font-bold text-gray-scale-300 mb-[7px]">카테고리</div>
+              <div className="text-[15px]">주변 맛집 이름</div>
+            </div>
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="15" cy="15" r="15" fill="#D9D9D9" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M14.9999 8.38854C15.8749 7.49946 17.0144 7 18.2039 7C19.4884 7 20.7146 7.58238 21.6124 8.60881C22.5038 9.62713 23 10.9998 23 12.4254C23 13.851 22.5037 15.2238 21.6124 16.242C21.0197 16.9194 20.4279 17.6122 19.8331 18.3086C18.6249 19.723 17.4038 21.1526 16.1353 22.4995L16.1324 22.5026C15.4782 23.187 14.4422 23.1621 13.816 22.4465L8.38716 16.2419C6.53761 14.1281 6.53761 10.7227 8.38716 8.6089C10.1971 6.54038 13.1115 6.46693 14.9999 8.38854Z"
+                fill="#B4B4B4"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/public/images/camping/camping-popular-dummy1.png"
+            alt="인근 명소 이미지"
+            className="w-[228px] h-[228px] rounded-xl mb-[10px]"
+          />
+          <div className="flex justify-between items-center">
+            <div>
+              <div className="text-[13px] font-bold text-gray-scale-300 mb-[7px]">카테고리</div>
+              <div className="text-[15px]">주변 맛집 이름</div>
+            </div>
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="15" cy="15" r="15" fill="#D9D9D9" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M14.9999 8.38854C15.8749 7.49946 17.0144 7 18.2039 7C19.4884 7 20.7146 7.58238 21.6124 8.60881C22.5038 9.62713 23 10.9998 23 12.4254C23 13.851 22.5037 15.2238 21.6124 16.242C21.0197 16.9194 20.4279 17.6122 19.8331 18.3086C18.6249 19.723 17.4038 21.1526 16.1353 22.4995L16.1324 22.5026C15.4782 23.187 14.4422 23.1621 13.816 22.4465L8.38716 16.2419C6.53761 14.1281 6.53761 10.7227 8.38716 8.6089C10.1971 6.54038 13.1115 6.46693 14.9999 8.38854Z"
+                fill="#B4B4B4"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/public/images/camping/camping-popular-dummy1.png"
+            alt="인근 명소 이미지"
+            className="w-[228px] h-[228px] rounded-xl mb-[10px]"
+          />
+          <div className="flex justify-between items-center">
+            <div>
+              <div className="text-[13px] font-bold text-gray-scale-300 mb-[7px]">카테고리</div>
+              <div className="text-[15px]">주변 맛집 이름</div>
+            </div>
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="15" cy="15" r="15" fill="#D9D9D9" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M14.9999 8.38854C15.8749 7.49946 17.0144 7 18.2039 7C19.4884 7 20.7146 7.58238 21.6124 8.60881C22.5038 9.62713 23 10.9998 23 12.4254C23 13.851 22.5037 15.2238 21.6124 16.242C21.0197 16.9194 20.4279 17.6122 19.8331 18.3086C18.6249 19.723 17.4038 21.1526 16.1353 22.4995L16.1324 22.5026C15.4782 23.187 14.4422 23.1621 13.816 22.4465L8.38716 16.2419C6.53761 14.1281 6.53761 10.7227 8.38716 8.6089C10.1971 6.54038 13.1115 6.46693 14.9999 8.38854Z"
+                fill="#B4B4B4"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/public/images/camping/camping-popular-dummy1.png"
+            alt="인근 명소 이미지"
+            className="w-[228px] h-[228px] rounded-xl mb-[10px]"
+          />
+          <div className="flex justify-between items-center">
+            <div>
+              <div className="text-[13px] font-bold text-gray-scale-300 mb-[7px]">카테고리</div>
+              <div className="text-[15px]">주변 맛집 이름</div>
+            </div>
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="15" cy="15" r="15" fill="#D9D9D9" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M14.9999 8.38854C15.8749 7.49946 17.0144 7 18.2039 7C19.4884 7 20.7146 7.58238 21.6124 8.60881C22.5038 9.62713 23 10.9998 23 12.4254C23 13.851 22.5037 15.2238 21.6124 16.242C21.0197 16.9194 20.4279 17.6122 19.8331 18.3086C18.6249 19.723 17.4038 21.1526 16.1353 22.4995L16.1324 22.5026C15.4782 23.187 14.4422 23.1621 13.816 22.4465L8.38716 16.2419C6.53761 14.1281 6.53761 10.7227 8.38716 8.6089C10.1971 6.54038 13.1115 6.46693 14.9999 8.38854Z"
+                fill="#B4B4B4"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
