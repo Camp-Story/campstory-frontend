1cb2438bd78ada21e58e26104e9e89527f6ebe2d
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2025-01-23 15:27:22 +0900

    add:filter

--- src/app/routes/SHOPPING_ROUTES.tsx
+++ src/app/routes/SHOPPING_ROUTES.tsx
@@ -1,5 +1,7 @@
 import { PATH } from "@constants/path";
+
 import ShoppingMain from "@pages/ShoppingMain";
+import ShoppingSearch from "@pages/ShoppingSearch";
 
 const SHOPPING_ROUTES = [
   {
@@ -8,7 +10,7 @@ const SHOPPING_ROUTES = [
   },
   {
     path: PATH.shoppingSearch,
-    element: <>shopping search</>,
+    element: <ShoppingSearch />,
   },
   {
     path: PATH.shoppingInfoPath,
