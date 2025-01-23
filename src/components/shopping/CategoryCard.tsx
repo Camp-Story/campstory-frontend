d7adb41898507e8d29fc00cb6b597d8489eb1a3c
marie <marie061313@gmail.com>
2025-01-23 13:51:24 +0900

    feat: ShoppingMain Category 섹션 구현

---
+++ src/components/shopping/CategoryCard.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router";
+
+interface CategoryCardProps {
+  src: string;
+  catName: string;
+  path: string;
+}
+
+export default function CategoryCard({ path, src, catName }: CategoryCardProps) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <button
+        onClick={() => navigate(path)}
+        className="flex flex-col items-center justify-center py-[40px] px-[60px] bg-white drop-shadow-custom rounded-xl hover:shadow-lg transition"
+      >
+        <div className="w-15 bg-gray-200 rounded-full">
+          {/* 아이콘/이미지 */}
+          <img src={src} alt="category" className="m-3" />
+        </div>
+        {/* 카테고리 이름 */}
+        <span className="text-body1 text-gray-scale-400 mt-3">{catName}</span>
+      </button>
+    </>
+  );
+}
