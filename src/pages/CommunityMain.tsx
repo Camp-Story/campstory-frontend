0b400f8bcdf0a7b59c0d3de8218339fc0c61c632
soyoung125 <sypark041754@gmail.com>
2025-01-23 17:22:28 +0900

    feat: create community header

--- src/pages/CommunityMain.tsx
+++ src/pages/CommunityMain.tsx
@@ -1,10 +1,34 @@
 import SearchInput from "@components/common/SearchInput";
+import Tag from "@components/community/Tag";
+import { PATH } from "@constants/path";
+import { useNavigate } from "react-router";
 
 export default function CommunityMain() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="mt-[100px] mb-[60px]">
         <SearchInput handleSubmit={() => alert("submit!")} />
+      </div>
+
+      <div className="flex gap-[50px] justify-between items-center mb-[30px]">
+        <div className="flex gap-5 text-sm">
+          <span className="text-gray-scale-400 font-bold">최신순</span>
+          <span className="text-gray-scale-300">인기순</span>
+        </div>
+
+        <div className="flex gap-[5px]">
+          <Tag tag="clean" />
+          <Tag tag="kind" />
+          <Tag tag="conveny" />
+        </div>
+
+        <button
+          className="px-3 py-2 ml-auto text-white text-sm bg-primary-500 rounded"
+          onClick={() => navigate(PATH.communityCreate)}
+        >
+          글 작성하기
+        </button>
       </div>
     </>
   );
