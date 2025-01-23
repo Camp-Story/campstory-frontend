63090350aba924b4b70ee5100043cd86155fd075
marie <marie061313@gmail.com>
2025-01-23 12:21:53 +0900

    fix: CampingMain Banner 수정

--- src/components/camping/campingMain/Banner.tsx
+++ src/components/camping/campingMain/Banner.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 export default function Banner() {
   return (
     <div className="relative">
-      <img src="/images/food/food-banner.png" alt="food banner" className="w-full" />
+      <img src="/images/camping/camping-banner.png" alt="camping banner" className="w-full" />
       <SearchBar className="absolute bottom-[60px] left-[50%] -translate-x-[50%]" />
     </div>
   );
