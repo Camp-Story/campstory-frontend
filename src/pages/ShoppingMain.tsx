68223f1566dedcb1fce457d70538951652d714e9
marie <marie061313@gmail.com>
2025-01-23 14:24:01 +0900

    fix: image 파일 shopping 오타 수정

--- src/pages/ShoppingMain.tsx
+++ src/pages/ShoppingMain.tsx
@@ -8,7 +8,7 @@ export default function ShoppingMain() {
     <div className="flex flex-col gap-[60px]">
       {/* Banner */}
       <div className="relative">
-        <img src="/images/shpping/shopping-banner.png" alt="banner" className="w-full" />
+        <img src="/images/shopping/shopping-banner.png" alt="banner" className="w-full" />
         <SearchBar
           handleSubmit={() => alert("search!")}
           className="absolute bottom-[60px] left-[50%] -translate-x-[50%]"
@@ -20,32 +20,32 @@ export default function ShoppingMain() {
         <SubTitle>카테고리별 분류</SubTitle>
         <div className="flex gap-0 justify-between">
           <CategoryCard
-            src="/images/shpping/food.png"
+            src="/images/shopping/food.png"
             catName="먹거리"
             path={PATH.shoppingInfoPath}
           />
           <CategoryCard
-            src="/images/shpping/tent.png"
+            src="/images/shopping/tent.png"
             catName="텐트"
             path={PATH.shoppingInfoPath}
           />
           <CategoryCard
-            src="/images/shpping/sleeping-bag.png"
+            src="/images/shopping/sleeping-bag.png"
             catName="침낭"
             path={PATH.shoppingInfoPath}
           />
           <CategoryCard
-            src="/images/shpping/brazier.png"
+            src="/images/shopping/brazier.png"
             catName="화로대"
             path={PATH.shoppingInfoPath}
           />
           <CategoryCard
-            src="/images/shpping/camping-furniture.png"
+            src="/images/shopping/camping-furniture.png"
             catName="캠핑 가구"
             path={PATH.shoppingInfoPath}
           />
           <CategoryCard
-            src="/images/shpping/bonfire.png"
+            src="/images/shopping/bonfire.png"
             catName="소모품"
             path={PATH.shoppingInfoPath}
           />
