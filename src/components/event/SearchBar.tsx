8c875fa2136b8819a358e19cf4e597e200f396e0
leejihwan99 <lbwlimes@gmail.com>
2025-01-24 14:07:31 +0900

    fix: 검색바 아웃라인과 서브타이틀 색상 변경

--- src/components/event/SearchBar.tsx
+++ src/components/event/SearchBar.tsx
@@ -11,7 +11,7 @@ export default function SearchBar({ className }: SearchBarProps) {
     <div className={className}>
       <input
         placeholder="검색어를 입력해주세요."
-        className="relative py-3 px-6 w-[892px] rounded-full placeholder-gray-scale-300 focus:outline-green-700"
+        className="relative py-3 px-6 w-[892px] rounded-full placeholder-gray-scale-300 focus:outline-secondary-300"
       />
       <button
         onClick={() => navigate(PATH.campingSearch)}
