484152d4ec591ddeea52cfbc0f45fe8b037abab3
marie <marie061313@gmail.com>
2025-01-24 11:08:34 +0900

    feat: mypage router setting

--- src/app/routes/AUTH_ROUTES.tsx
+++ src/app/routes/AUTH_ROUTES.tsx
@@ -1,4 +1,3 @@
-import AuthMyPageLayout from "@components/layouts/AuthMyPageLayout";
 import { PATH } from "@constants/path";
 
 const AUTH_ROUTES = [
@@ -9,26 +8,6 @@ const AUTH_ROUTES = [
   {
     path: PATH.login,
     element: <>login</>,
-  },
-  {
-    path: PATH.mypage,
-    element: <AuthMyPageLayout />,
-  },
-  {
-    path: PATH.information,
-    element: <>mypage information</>,
-  },
-  {
-    path: PATH.reservedList,
-    element: <>mypage reservedList</>,
-  },
-  {
-    path: PATH.bookmark,
-    element: <>mypage bookmark</>,
-  },
-  {
-    path: PATH.activities,
-    element: <>mypage activities</>,
   },
 ];
 
