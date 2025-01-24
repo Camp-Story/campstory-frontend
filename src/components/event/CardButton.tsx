bcda7787b690e607214d66f21ca3e851c2510fa9
leejihwan99 <lbwlimes@gmail.com>
2025-01-24 15:22:08 +0900

    fix: pritter 수정

--- src/components/event/CardButton.tsx
+++ src/components/event/CardButton.tsx
@@ -1,14 +1,13 @@
 interface CardButtonProps {
-    src: string;
-    children: string;
+  src: string;
+  children: string;
 }
 
 export default function CardButton({ src, children }: CardButtonProps) {
-    return (
-        <div>
-            <img src={src} alt="CampingImage" className="w-[300px]" />
-            <div className="text-[26px] font-bold my-4">{children}</div>
-        </div>
-    );
+  return (
+    <div>
+      <img src={src} alt="CampingImage" className="w-[300px]" />
+      <div className="text-[26px] font-bold my-4">{children}</div>
+    </div>
+  );
 }
-  
