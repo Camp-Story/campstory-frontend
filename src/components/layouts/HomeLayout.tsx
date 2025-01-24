3406e01ead979c2435f9b7237b9f12f9aa8caf80
soyoung125 <sypark041754@gmail.com>
2025-01-24 11:38:13 +0900

    refactor: remove footer to auth pages (#77)

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -3,10 +3,16 @@ import "swiper/css";
 
 export default function HomeLayout() {
   return (
-    <div>
-      <main className="px-main">
-        <Outlet />
+    <>
+      <main className="w-[1440px] m-auto">
+        <div className="px-main">
+          <Outlet />
+        </div>
       </main>
-    </div>
+
+      <footer className="h-52 bg-gray-200 mt-28">
+        <div className="w-[1440px] m-auto px-main py-7">footer</div>
+      </footer>
+    </>
   );
 }
--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -4,16 +4,13 @@ import "swiper/css";
 
 export default function RootLayout() {
   return (
-    <div className="h-dvh">
-      <div className="font-sans w-[1440px] m-auto">
+    <div className="min-h-dvh">
+      <div className="font-sans">
         <header className="sticky top-0 w-full bg-gray-scale-0 px-main py-3 z-20">
           <MainNavigation />
         </header>
         <Outlet />
       </div>
-      <footer className="h-52 bg-gray-200 mt-28">
-        <div className="w-[1440px] m-auto px-main py-7">footer</div>
-      </footer>
     </div>
   );
 }
