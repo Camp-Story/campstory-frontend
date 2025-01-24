f685f21de7787aa252d483f90f6379d997c88caf
soyoung125 <sypark041754@gmail.com>
2025-01-24 12:38:04 +0900

    refactor: update gitignore (#80)

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+interface ImportMeta {
+  env: {
+    VITE_API_BASE_URL: string;
+    VITE_GO_CAMPING_API_KEY: string;
+    VITE_TOUR_API_KEY: string;
+    VITE_NAVER_API_ID_KEY: string;
+    VITE_NAVER_SECRET_KEY: string;
+    VITE_KAKAO_API_KEY: string;
+  };
+}
