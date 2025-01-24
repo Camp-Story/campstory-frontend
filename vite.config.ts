21fb2a2ee8ef00a6fa71de7aabaf25bdc8645a0a
Sean <joajoa70584@gmail.com>
2025-01-24 15:02:48 +0900

    feat: add server proxy configuration for API requests to Naver OpenAPI

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/v1": {
+        target: "https://openapi.naver.com",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
