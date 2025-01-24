2f999f48aa47f02d78a0545ebda8b10c62bb00db
marie <marie061313@gmail.com>
2025-01-24 16:09:59 +0900

    design: change QuestionCard font weight

--- src/components/community/question/QuestionCard.tsx
+++ src/components/community/question/QuestionCard.tsx
@@ -10,7 +10,7 @@ export default function QuestionCard() {
         <div className="flex flex-col gap-4 bg-gray-scale-0 w-[600px] p-[20px] rounded-sm drop-shadow-custom">
           <div className="flex gap-4 items-center">
             <div className="text-primary-500 font-bold text-[26px]">Q.</div>
-            <div className="text-body1 text-gray-scale-400">
+            <div className="text-body1 font-medium text-gray-scale-400">
               안녕하세요~ 보통 캠핑장 매점에서 구매하는 주류는 가격이 어떻게 될까용?
             </div>
           </div>
