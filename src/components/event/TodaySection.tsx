4b6ab87c8880a77d78676afa4e74ae61a4a3700b
leejihwan99 <lbwlimes@gmail.com>
2025-01-24 15:26:02 +0900

    refactor: todaysection 리팩토링

--- src/components/event/TodaySection.tsx
+++ src/components/event/TodaySection.tsx
@@ -1,35 +1,38 @@
-export default function TodaySection() {
+import TodaySectionProps from "types/TodaySectionProps";
+import { Link } from "react-router";
+
+export default function TodaySection({ src, title, subTitle, tag, path }: TodaySectionProps) {
   return (
-    <div className="flex mx-[106px] my-[100px]">
+    <section className="flex mx-[106px] my-[100px] h-[400px]">
       <div className="flex-1 mr-[70px]">
-        <div className="text-highlight text-[52px] text-gray-scale-500 mb-[20px]">
-          새해를 여는 밝은 희망, 감악산 해맞이
-        </div>
-        <div className="text-[20px] text-gray-scale-300 mb-14">
-          2025년, 새해의 첫 태양과 함께 희망을 품어보세요! 감악산 해맞이에서 풍물패 공연, 소원 적기,
-          떡국 시식 등 다채로운 즐거움을 만나보세요
+        <div className="text-highlight text-[52px] text-gray-scale-500 mb-[20px]">{title}</div>
+        <div className="text-[20px] text-gray-scale-300 mb-14 max-h-[72px] overflow-y-auto">
+          {subTitle}
         </div>
         <div className="flex items-center text-[20px] text-gray-scale-300 font-bold mb-12">
-          <img
-            src="/images/festival/event-tag-star.png"
-            alt="별 모양 태그"
-            className="inline-block mr-2"
-          />
-          핫플레이스
+          {tag.map((t, index) => (
+            <div key={index} className="flex items-center mr-4">
+              <img
+                src="/images/festival/event-tag-star.png"
+                alt="별 모양 태그"
+                className="inline-block mr-2"
+              />
+              <span>{t}</span>
+            </div>
+          ))}
         </div>
-        <div>
-          <button className="border p-2 rounded-xl text-gray-scale-400 font-bold bg-gray-scale-100 w-[302px] h-[58px]">
+        <div className="pb-4">
+          <Link
+            to={path}
+            className="border p-2 rounded-xl text-gray-scale-400 font-bold bg-gray-scale-100 w-[302px] h-[58px] flex justify-center items-center shadow-sm hover:bg-gray-300 transition"
+          >
             더 알아보기
-          </button>
+          </Link>
         </div>
       </div>
       <div className="flex-1 flex justify-center">
-        <img
-          src="/images/festival/event-dummy.png"
-          alt="오늘의 행사 이미지"
-          className="max-w-full"
-        />
+        <img src={src} alt="오늘의 행사 이미지" className="max-w-full" />
       </div>
-    </div>
+    </section>
   );
 }
