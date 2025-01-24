58471c18131c0e4e8a4971cada321faf13e07790
marie <marie061313@gmail.com>
2025-01-24 21:39:05 +0900

    feat: create Comment component

--- src/pages/QuestionDetail.tsx
+++ src/pages/QuestionDetail.tsx
@@ -1,20 +1,25 @@
 import CommentInput from "@components/community/question/CommentInput";
 import QuestionDetailCard from "@components/community/question/QuestionDetailCard";
-
+import Comment from "@components/community/question/Comment";
 export default function QuestionDetail() {
   return (
-    <div>
-      <div className="mt-[100px]">
+    <div className="w-[832px] mx-auto mt-[100px] flex flex-col gap-[30px]">
+      <div>
         <QuestionDetailCard />
       </div>
-
       <div className="flex flex-col gap-4">
-        <div className="text-sub-title text-gray-scale-400 font-bold mt-[30px]">댓글</div>
+        <div className="text-sub-title text-gray-scale-400 font-bold">댓글</div>
         <CommentInput
           handleSubmit={() => {
             alert("submit!");
           }}
         />
+      </div>
+      <div className="flex flex-col gap-[40px]">
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </div>
   );
