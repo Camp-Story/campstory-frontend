1e3dc326c1ebedff06bb500ed2b404b187b560f5
marie <marie061313@gmail.com>
2025-01-24 15:33:37 +0900

    feat: create QuestionCard component

---
+++ src/components/community/question/QuestionCard.tsx
@@ -0,0 +1,30 @@
+import AdditionalInfo from "../AdditionalInfo";
+import UserProfile from "../UserProfile";
+
+export default function QuestionCard() {
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <UserProfile nickname="사용자 닉네임" profileUrl="" />
+        {/* card */}
+        <div className="flex flex-col gap-4 bg-gray-scale-0 w-[600px] p-[20px] drop-shadow-custom">
+          <div className="flex gap-4 items-center">
+            <div className="text-primary-500 font-bold text-[26px]">Q.</div>
+            <div className="text-body1 text-gray-scale-400">
+              안녕하세요~ 보통 캠핑장 매점에서 구매하는 주류는 가격이 어떻게 될까용?
+            </div>
+          </div>
+          <AdditionalInfo
+            bookmarked
+            isLiked={false}
+            likeCount={10}
+            viewCount={115}
+            time="24분"
+            handleClickBookmark={() => alert("click bookmark")}
+            handleClickLike={() => alert("click like")}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
