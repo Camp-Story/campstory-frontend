52cc31e3588654ca0c68c45420379ca2d7fccbc9
soyoung125 <sypark041754@gmail.com>
2025-01-24 15:37:26 +0900

    refactor: seperate image uploader component (#80)

--- src/pages/CommunityCreate.tsx
+++ src/pages/CommunityCreate.tsx
@@ -1,42 +1,12 @@
 import AreaCard from "@components/community/community/AreaCard";
 import Tag from "@components/community/Tag";
-import ImageIcon from "@components/community/icons/ImageIcon";
+import ImageUploader from "@components/community/communityCreate/ImageUploader";
 
 export default function CommunityCreate() {
   return (
     <div className="w-[618px] mx-auto mt-[52px]">
       <h1 className="mb-[30px] text-[26px] font-bold">게시글 쓰기</h1>
-      <div className="flex gap-[11px] mb-10">
-        <div>
-          <input
-            type="file"
-            id="image"
-            accept="image/png, image/gif, image/jpeg"
-            className="hidden"
-          />
-          <label
-            htmlFor="image"
-            className="flex flex-col gap=[5px] justify-center items-center rounded w-[110px] h-[110px] bg-gray-scale-100"
-          >
-            <ImageIcon />
-            <span className="text-[15px] text-gray-scale-400">4/10</span>
-          </label>
-        </div>
-        <div className="snap-x flex gap-[11px] snap-mandatory w-full overflow-x-scroll scrollbar-hidden">
-          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
-            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
-          </div>
-          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
-            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
-          </div>
-          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
-            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
-          </div>
-          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
-            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
-          </div>
-        </div>
-      </div>
+      <ImageUploader />
 
       <div className="flex flex-col gap-[15px]">
         <h2 className="text-sub-title">장소 선택</h2>
