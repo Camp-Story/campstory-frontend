5c12e1e34c09b77f7b94720c845396a1292cb10d
soyoung125 <sypark041754@gmail.com>
2025-01-24 15:13:16 +0900

    feat: add image uploader component (#80)

--- src/pages/CommunityCreate.tsx
+++ src/pages/CommunityCreate.tsx
@@ -1,12 +1,37 @@
+import ImageIcon from "@components/community/icons/ImageIcon";
+
 export default function CommunityCreate() {
   return (
     <div className="w-[618px] mx-auto mt-[70px]">
       <h1 className="mb-[30px] text-[26px] font-bold">게시글 쓰기</h1>
       <div className="flex gap-[11px]">
-        <div className="rounded flex flex-col "></div>
-        <div className="snap-x">
-          <div className="snap-end ">
-            <img src="/img/vacation-01.jpg" />
+        <div>
+          <input
+            type="file"
+            id="image"
+            accept="image/png, image/gif, image/jpeg"
+            className="hidden"
+          />
+          <label
+            htmlFor="image"
+            className="flex flex-col gap=[5px] justify-center items-center rounded w-[110px] h-[110px] bg-gray-scale-100"
+          >
+            <ImageIcon />
+            <span className="text-[15px] text-gray-scale-400">4/10</span>
+          </label>
+        </div>
+        <div className="snap-x flex gap-[11px] snap-mandatory w-full overflow-x-scroll scrollbar-hidden">
+          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
+            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
+          </div>
+          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
+            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
+          </div>
+          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
+            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
+          </div>
+          <div className="snap-end shrink-0 rounded overflow-hidden w-[110px] h-[110px]">
+            <img src="https://placehold.co/110x110?text=CAMP+STORY" alt="community image" />
           </div>
         </div>
       </div>
