ab034527f470a9cc05988f821e4e9fb0ce8b165e
Sean <joajoa70584@gmail.com>
2025-01-27 15:50:42 +0900

    refactor: shopping-related types into NaverResponse type

---
+++ src/types/NaverResponse.ts
@@ -0,0 +1,26 @@
+export interface NaverProductResponse {
+  title: string;
+  link: string;
+  image: string;
+  lprice: string;
+  hprice: string;
+  mallName: string;
+  productId: string;
+  productType: string;
+  brand: string;
+  maker: string;
+  category1: string;
+  category2: string;
+  category3: string;
+  category4: string;
+}
+
+export interface NaverSearchResponse {
+  items: NaverProductResponse[];
+}
+
+export interface ProductCardProps {
+  product: NaverProductResponse;
+  bookmarked?: boolean;
+  handleClickBookmark?: () => void;
+}
