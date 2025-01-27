f11848329caf80169c1fbb668536c5214e04dd78
marie <marie061313@gmail.com>
2025-01-27 14:07:50 +0900

    feat: contentId에 따른 상세페이지 연결 및 이미지 url 전달하기

--- src/components/detail/SpotAboutSection.tsx
+++ src/components/detail/SpotAboutSection.tsx
@@ -1,17 +1,21 @@
-interface CampingInfoProps<T extends string = string> {
-  [key: string]: T;
-}
+// interface CampingInfoProps<T extends string = string> {
+//   [key: string]: T;
+// }
 
 interface CampingDetailProps {
   shortIntro: string;
   description: string;
-  campingInfo: CampingInfoProps;
+  sbrsEtc: string;
+  posblFcltyCl: string;
+  homepage: string;
 }
 
 export default function SpotAboutSection({
   shortIntro,
   description,
-  // campingInfo,
+  sbrsEtc,
+  posblFcltyCl,
+  homepage,
 }: CampingDetailProps) {
   return (
     <section className="grid grid-cols-12 mb-14">
@@ -25,7 +29,7 @@ export default function SpotAboutSection({
             </span>
           )}
           <p className="text-body1 text-gray-scale-400">
-            {description}
+            {description && description}
             담양군 봉산면 기곡리에 위치한 힐링파크 담양점은 도심과 떨어져 조용히 숲과 자연에서
             자유로이 쉴수 있는 힐링파크다. 이곳은 기존 봉산관광농원이라는 이름으로 1998년 농림부
             1등급 판정을 받은 시설로 어느 휴양지에서도 느끼지 못했던 색다른 휴식과 즐거움을 동시에
@@ -39,18 +43,19 @@ export default function SpotAboutSection({
             <li className="mb-3">
               <span className="block text-body1 text-gray-scale-400">
                 <strong className="mr-4 text-gray-scale-500">부대시설</strong>전기,무선인터넷,
-                장작판매
+                장작판매 {sbrsEtc}
               </span>
             </li>
             <li className="mb-3">
               <span className="block text-body1 text-gray-scale-400">
                 <strong className="mr-4 text-gray-scale-500">주변시설</strong>
-                운동장,강/물놀이,농어촌체험시설
+                운동장,강/물놀이,농어촌체험시설 {posblFcltyCl}
               </span>
             </li>
             <li className="mb-3">
               <span className="block text-body1 text-gray-scale-400">
-                <strong className="mr-4 text-gray-scale-500">홈페이지</strong>www.campstory.com
+                <strong className="mr-4 text-gray-scale-500">홈페이지</strong>www.campstory.com{" "}
+                {homepage}
               </span>
             </li>
           </ul>
