1071ee76b729c2970f6be3284fcaf5b013c5df9d
marie <marie061313@gmail.com>
2025-01-27 11:36:13 +0900

    feat: connect GoCamping API

--- src/pages/CampingSearch.tsx
+++ src/pages/CampingSearch.tsx
@@ -5,8 +5,124 @@ import SearchMap from "@components/common/search/SearchMap";
 import SearchInput from "@components/common/SearchInput";
 
 import { AREA, CAMPING_CATEGORY } from "@constants/filters";
+import { goCampingInstance } from "@utils/axiosInstance";
+import { useEffect, useState } from "react";
+
+interface campingDataResponse {
+  wtrplCo: string;
+  brazierCl: string;
+  sbrsCl: string;
+  sbrsEtc: string;
+  posblFcltyCl: string;
+  posblFcltyEtc: string;
+  clturEventAt: string;
+  clturEvent: string;
+  exprnProgrmAt: string;
+  hvofBgnde: string;
+  caravAcmpnyAt: string;
+  toiletCo: string;
+  swrmCo: string;
+  hvofEnddle: string;
+  featureNm: string;
+  induty: string;
+  lctCl: string;
+  doNm: string;
+  sigunguNm: string;
+  zipcode: string;
+  addr1: string;
+  addr2: string;
+  mapX: string;
+  mapY: string;
+  direction: string;
+  tel: string;
+  homepage: string;
+  resveUrl: string;
+  resveCl: string;
+  manageNmpr: string;
+  gnrlSiteCo: string;
+  autoSiteCo: string;
+  glampSiteCo: string;
+  caravSiteCo: string;
+  indvdlCaravSiteCo: string;
+  sitedStnc: string;
+  siteMg1Width: string;
+  siteMg2Width: string;
+  siteMg3Width: string;
+  siteMg1Vrticl: string;
+  siteMg2Vrticl: string;
+  siteMg3Vrticl: string;
+  siteMg1Co: string;
+  siteMg2Co: string;
+  siteMg3Co: string;
+  siteBottomCl1: string;
+  siteBottomCl2: string;
+  siteBottomCl3: string;
+  siteBottomCl4: string;
+  siteBottomCl5: string;
+  tooltip: string;
+  glampInnerFclty: string;
+  caravInnerFclty: string;
+  prmisnDe: string;
+  operPdCl: string;
+  operDeCl: string;
+  trlerAcmpnyAt: string;
+  intro: string;
+  allar: string;
+  insrncAt: string;
+  trsagntNo: string;
+  exprnProgrm: string;
+  extshrCo: string;
+  frprvtWrppCo: string;
+  frprvtSandCo: string;
+  fireSensorCo: string;
+  themaEnvrnCl: string;
+  mangeDivNm: string;
+  mgcDiv: string;
+  manageSttus: string;
+  eqpmnLendCl: string;
+  animalCmgCl: string;
+  tourEraCl: string;
+  firstImageUrl: string;
+  createdtime: string;
+  modifiedtime: string;
+  contentId: string;
+  facltNm: string;
+  lineIntro: string;
+  bizrno: string;
+  facltDivNm: string;
+}
 
 export default function CampingSearch() {
+  const [campingData, setCampingData] = useState<campingDataResponse[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchCampingData = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await goCampingInstance.get("/basedList", {
+        params: {
+          numOfRows: 10,
+          pageNo: 1,
+        },
+      });
+      console.log(response.data.response.body.items.item);
+      setCampingData(response.data.response.body.items.item);
+      console.log(Array.isArray(campingData));
+      console.log(typeof campingData);
+    } catch (error) {
+      setError("캠핑 데이터를 가져오는 중 오류가 발생했습니다.");
+      console.error("Error fetching camping data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCampingData();
+  }, []);
+
   return (
     <>
       <div className="mt-[100px] mb-[60px]">
@@ -33,44 +149,22 @@ export default function CampingSearch() {
         </div>
 
         <div className="flex flex-col gap-[30px]">
-          <h2 className="text-[26px] font-bold text-gray-scale-400">'캠핑' 검색 결과 14개</h2>
+          <h2 className="text-[26px] font-bold text-gray-scale-400">'캠핑' 검색 결과 {}개</h2>
+          {isLoading && "로딩중.."}
           <div className="grid grid-cols-2 gap-x-5 gap-y-[30px]">
-            <SearchCard
-              img="/images/camping/searchCamping.png"
-              bookmarked={false}
-              category="오토캠핑•펜션"
-              handleClick={() => alert("click")}
-              handleClickBookmark={() => alert("bookmark")}
-              location="경기도 가평군 가화로 1364-59"
-              title="가평미라몬티풀빌라펜션"
-            />
-            <SearchCard
-              img="/images/camping/searchCamping.png"
-              bookmarked={true}
-              category="오토캠핑•펜션"
-              handleClick={() => alert("click")}
-              handleClickBookmark={() => alert("bookmark")}
-              location="경기도 가평군 가화로 1364-59"
-              title="가평미라몬티풀빌라펜션"
-            />
-            <SearchCard
-              img="/images/camping/searchCamping.png"
-              bookmarked={false}
-              category="오토캠핑•펜션"
-              handleClick={() => alert("click")}
-              handleClickBookmark={() => alert("bookmark")}
-              location="경기도 가평군 가화로 1364-59"
-              title="가평미라몬티풀빌라펜션"
-            />
-            <SearchCard
-              img="/images/camping/searchCamping.png"
-              bookmarked={false}
-              category="오토캠핑•펜션"
-              handleClick={() => alert("click")}
-              handleClickBookmark={() => alert("bookmark")}
-              location="경기도 가평군 가화로 1364-59"
-              title="가평미라몬티풀빌라펜션"
-            />
+            {error}
+            {campingData.length !== 0 &&
+              campingData.map((item) => (
+                <SearchCard
+                  img={item.firstImageUrl}
+                  bookmarked={false}
+                  category={item.induty}
+                  handleClick={() => alert("click")}
+                  handleClickBookmark={() => alert("bookmark")}
+                  location={item.addr1}
+                  title={item.facltNm}
+                />
+              ))}
           </div>
         </div>
       </div>
