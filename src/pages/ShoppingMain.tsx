233e533795e2d62c6f2af1eae2bdaab2c5b5947f
Sean <joajoa70584@gmail.com>
2025-01-27 15:57:26 +0900

    refactor: Update dummy code and adjust component props accordingly

--- src/pages/ShoppingMain.tsx
+++ src/pages/ShoppingMain.tsx
@@ -9,101 +9,202 @@ import ProductCard from "@components/shopping/ProductCard";
 
 const RecommendProductData = [
   {
-    id: 1,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/photo-review-dummy-1.png",
+    brand: "코베아",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_5242096/52420960636.20250110122548.jpg",
+    link: "https://search.shopping.naver.com/catalog/52420960636",
+    lprice: "489000",
+    maker: "코베아",
+    mallName: "네이버",
+    productId: "52420960636",
+    productType: "1",
+    title: "코베아 네스트 W <b>텐트</b> 4인용 브라운",
   },
   {
-    id: 2,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/photo-review-dummy-1.png",
+    brand: "쿠디",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_5257910/52579100578.20250120171356.jpg",
+    link: "https://search.shopping.naver.com/catalog/52579100578",
+    lprice: "1990000",
+    maker: "쿠디",
+    mallName: "네이버",
+    productId: "52579100578",
+    productType: "1",
+    title: "쿠디 파밀리아 에어<b>텐트</b> 바닥 일체형 베이지",
   },
 ];
 
 const NewProductData = [
   {
-    id: 1,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item01.png",
+    brand: "쿠디",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_5261387/52613874085.20250122172801.jpg",
+    link: "https://search.shopping.naver.com/catalog/52613874085",
+    lprice: "359000",
+    maker: "쿠디",
+    mallName: "네이버",
+    productId: "52613874085",
+    productType: "1",
+    title: "쿠디 에어<b>텐트</b> 5.8 베이지",
   },
   {
-    id: 2,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item02.png",
+    brand: "코베아",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_3662609/36626094620.20221219153401.jpg",
+    link: "https://search.shopping.naver.com/catalog/36626094620",
+    lprice: "1165000",
+    maker: "",
+    mallName: "네이버",
+    productId: "36626094620",
+    productType: "1",
+    title: "코베아 고스트 팬텀 KECY9TO-06",
   },
   {
-    id: 3,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item03.png",
+    brand: "쿠디",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_4517138/45171382618.20240909145408.jpg",
+    link: "https://search.shopping.naver.com/catalog/45171382618",
+    lprice: "1679000",
+    maker: "쿠디",
+    mallName: "네이버",
+    productId: "45171382618",
+    productType: "1",
+    title: "쿠디 에어<b>텐트</b> 13.6",
   },
   {
-    id: 4,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item04.png",
+    brand: "아이두젠",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_5262980/52629806146.20250123144444.jpg",
+    link: "https://search.shopping.naver.com/catalog/52629806146",
+    lprice: "299000",
+    maker: "아이두젠",
+    mallName: "네이버",
+    productId: "52629806146",
+    productType: "1",
+    title: "아이두젠 옥타곤 원터치 <b>텐트</b> 쉘터 라이트그레이",
   },
   {
-    id: 5,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item05.png",
+    brand: "아이두젠",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_3146703/31467035618.20240823160702.jpg",
+    link: "https://search.shopping.naver.com/catalog/31467035618",
+    lprice: "299000",
+    maker: "아이두젠",
+    mallName: "네이버",
+    productId: "31467035618",
+    productType: "1",
+    title: "아이두젠 아마데우스 원터치 <b>텐트</b> 쉘터",
   },
 ];
 
 const DiscountProductData = [
   {
-    id: 1,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item01.png",
+    brand: "아이두젠",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_3146703/31467035618.20240823160702.jpg",
+    link: "https://search.shopping.naver.com/catalog/31467035618",
+    lprice: "299000",
+    maker: "아이두젠",
+    mallName: "네이버",
+    productId: "31467035618",
+    productType: "1",
+    title: "아이두젠 아마데우스 원터치 <b>텐트</b> 쉘터",
   },
   {
-    id: 2,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item02.png",
+    brand: "로티캠프",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_4612468/46124681618.20240306100132.jpg",
+    link: "https://search.shopping.naver.com/catalog/46124681618",
+    lprice: "419000",
+    maker: "로티",
+    mallName: "네이버",
+    productId: "46124681618",
+    productType: "1",
+    title: "로티 로티캠프 힐하우스 그늘막 원터치 <b>텐트</b> 3-4인용",
   },
   {
-    id: 3,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item03.png",
+    brand: "쿠디",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_5077322/50773227620.20241109133742.jpg",
+    link: "https://search.shopping.naver.com/catalog/50773227620",
+    lprice: "1279000",
+    maker: "쿠디",
+    mallName: "네이버",
+    productId: "50773227620",
+    productType: "1",
+    title: "쿠디 에어<b>텐트</b> 10.0",
   },
   {
-    id: 4,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item04.png",
+    brand: "스노우라인",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_2867937/28679375559.20210902120103.jpg",
+    link: "https://search.shopping.naver.com/catalog/28679375559",
+    lprice: "623200",
+    maker: "스노우라인",
+    mallName: "네이버",
+    productId: "28679375559",
+    productType: "1",
+    title: "스노우라인 새턴2룸 프로 터널형<b>텐트</b> 4인용",
   },
   {
-    id: 5,
-    brand: "브랜드 이름",
-    name: "시어플러스57 배틀큐 그릴",
-    discount: "30%",
-    price: "168,000원",
-    img: "/images/shopping/shoppingItems/item05.png",
+    brand: "로티캠프",
+    category1: "스포츠/레저",
+    category2: "캠핑",
+    category3: "텐트",
+    category4: "3-4인용",
+    hprice: "",
+    image: "https://shopping-phinf.pstatic.net/main_1118076/11180765626.20240805182225.jpg",
+    link: "https://search.shopping.naver.com/catalog/11180765626",
+    lprice: "78900",
+    maker: "로티",
+    mallName: "네이버",
+    productId: "11180765626",
+    productType: "1",
+    title: "로티 로티캠프 육각 돔 원터치 <b>텐트</b> 4윈도우 3-4인용",
   },
 ];
 
@@ -164,32 +265,28 @@ export default function ShoppingMain() {
           <CategoryCard
             src="/images/shopping/food.png"
             catName="먹거리"
-            path={PATH.shoppingInfoPath}
+            path={PATH.shoppingSearch}
           />
-          <CategoryCard
-            src="/images/shopping/tent.png"
-            catName="텐트"
-            path={PATH.shoppingInfoPath}
-          />
+          <CategoryCard src="/images/shopping/tent.png" catName="텐트" path={PATH.shoppingSearch} />
           <CategoryCard
             src="/images/shopping/sleeping-bag.png"
             catName="침낭"
-            path={PATH.shoppingInfoPath}
+            path={PATH.shoppingSearch}
           />
           <CategoryCard
             src="/images/shopping/brazier.png"
             catName="화로대"
-            path={PATH.shoppingInfoPath}
+            path={PATH.shoppingSearch}
           />
           <CategoryCard
             src="/images/shopping/camping-furniture.png"
             catName="캠핑 가구"
-            path={PATH.shoppingInfoPath}
+            path={PATH.shoppingSearch}
           />
           <CategoryCard
             src="/images/shopping/bonfire.png"
             catName="소모품"
-            path={PATH.shoppingInfoPath}
+            path={PATH.shoppingSearch}
           />
         </div>
       </div>
@@ -203,14 +300,9 @@ export default function ShoppingMain() {
         <div className="flex gap-2 justify-between items-center">
           {RecommendProductData.map((product) => (
             <RecommendCard
-              img={product.img}
+              key={product.productId}
+              product={product}
               bookmarked={false}
-              key={product.id}
-              brandName={product.brand}
-              discount={product.discount}
-              productName={product.name}
-              price={product.price}
-              handleClick={() => alert("click")}
               handleClickBookmark={() => alert("bookmark")}
             />
           ))}
@@ -226,13 +318,9 @@ export default function ShoppingMain() {
         <div className="flex gap-2 justify-between items-center">
           {NewProductData.map((product) => (
             <ProductCard
-              img={product.img}
+              key={product.productId}
+              product={product}
               bookmarked={false}
-              key={product.id}
-              brandName={product.brand}
-              productName={product.name}
-              price={product.price}
-              handleClick={() => alert("click")}
               handleClickBookmark={() => alert("bookmark")}
             />
           ))}
@@ -241,8 +329,8 @@ export default function ShoppingMain() {
 
       {/* Sale */}
       <div className="relative">
-        <div className="absolute bg-[#E8F6F1] h-[470px] w-screen -top-4 -left-[50%] -z-10"></div>
-        <div className="absolute bg-[#E8F6F1] h-[470px] -top-4 w-screen -z-10"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#E8F6F1] w-screen h-[450px] -z-10"></div>
+        {/* <div className="absolute bg-[#E8F6F1] h-[470px] -top-10 w-screen -z-10"></div> */}
         <div className="flex justify-between items-center">
           <SubTitle>최저가 득템!</SubTitle>
           <button className="text-info-500">더보기</button>
@@ -250,14 +338,9 @@ export default function ShoppingMain() {
         <div className="flex gap-2 justify-between items-center">
           {DiscountProductData.map((product) => (
             <ProductCard
-              img={product.img}
+              key={product.productId}
+              product={product}
               bookmarked={false}
-              key={product.id}
-              brandName={product.brand}
-              productName={product.name}
-              discount={product.discount}
-              price={product.price}
-              handleClick={() => alert("click")}
               handleClickBookmark={() => alert("bookmark")}
             />
           ))}
