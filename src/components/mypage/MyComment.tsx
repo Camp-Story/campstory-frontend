dc0e879e94e542715961da5ea3580698162395da
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2025-01-27 09:43:16 +0900

    fix: 컴포넌트들 이름 들 수정하였습니다.

--- src/components/mypage/MyComment.tsx
+++ src/components/mypage/MyComment.tsx
@@ -8,7 +8,7 @@ export default function MyComment() {
       <div className="flex gap-4 items-center">
         <div className="text-gray-scale-300">1달 전</div>
       </div>
-      <div className="text-gray-scale-400 font-medium ml-[40px] mt-3 leading-5">
+      <div className="text-gray-scale-400 font-medium ml-[40px] mt-3 leading-5 text-l">
         캠프하세요. 등유난로가 켜고 끌때 기름냄새는 당연한거고 연소중에는 냄새 안나요. 또 난로
         열기가 위로 올라가는거 위에 팬달거나 그러지말고 유동력 무동력 퍈을 사용햐서 순환시켜보세요
         급이 달라져요. 단점은 비싸다는거...
