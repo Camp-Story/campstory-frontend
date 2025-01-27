519ecadc0364533fcefe85aac60730272fa41d0a
Sean <joajoa70584@gmail.com>
2025-01-27 12:27:50 +0900

    feat: add handler for list sorting options

--- src/pages/ShoppingSearch.tsx
+++ src/pages/ShoppingSearch.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import SearchInput from "@components/common/SearchInput";
 import Category from "@components/shopping/Category";
 import ProductCardList from "@components/shopping/ProductCardList";
 import OrderRadio from "@components/community/OrderRadio";
 
 export default function ShoppingSearch() {
+  const [orderBy, setOrderBy] = useState("sim");
+  const handleOrderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrderBy(e.target.value);
+    console.log("Selected order:", e.target.value);
+  };
+
   return (
     <div className="mx-">
       <div className="mt-[100px]">
@@ -21,15 +28,15 @@ export default function ShoppingSearch() {
         />
         <Category path="소모품" src="/images/shopping/bonfire.png" catName="소모품" />
       </div>
-      <div className="flex mt-[64px] gap-5 text-[20px] ">
-        <OrderRadio label="판매인기순" value="popular" />
-        <OrderRadio label="높은금액순" value="highPrice" />
-        <OrderRadio label="낮은금액순" value="lowPrice" />
-        <OrderRadio label="최신순" value="recent" defaultChecked />
+      <div className="flex mt-[64px] gap-5 text-[18px] mb-10" onChange={handleOrderChange}>
+        <OrderRadio label="높은금액순" value="dsc" />
+        <OrderRadio label="낮은금액순" value="asc" />
+        <OrderRadio label="날짜순" value="date" />
+        <OrderRadio label="관련도순" value="sim" defaultChecked />
       </div>
 
       <div className="mb-[208px] mt-[10px] gap-4">
-        <ProductCardList />
+        <ProductCardList orderBy={orderBy} />
       </div>
     </div>
   );
