9827988defc3e57385cec70d0b2a1818df318cde
marie <marie061313@gmail.com>
2025-01-28 14:55:55 +0900

    feat: 인기캠핑장 리스트 상세 페이지 연동

---
+++ src/types/CampingDataResponse.ts
@@ -0,0 +1,18 @@
+interface campingDataResponse {
+  sbrsCl: string;
+  posblFcltyCl: string;
+  featureNm: string;
+  induty: string;
+  addr1: string;
+  addr2: string;
+  mapX: string;
+  mapY: string;
+  tel: string;
+  homepage: string;
+  firstImageUrl: string;
+  contentId: string;
+  facltNm: string;
+  lineIntro: string;
+}
+
+export default campingDataResponse;
