64888677e0a749e0f7317d23a51cc24968d3161b
Sean <joajoa70584@gmail.com>
2025-01-31 12:46:26 +0900

    feat: update UI of community page

--- src/pages/CommunityDetail.tsx
+++ src/pages/CommunityDetail.tsx
@@ -1,5 +1,7 @@
 import AdditionalInfo from "@components/community/AdditionalInfo";
 import AreaCard from "@components/community/community/AreaCard";
+import Comment from "@components/community/question/Comment";
+import CommentInput from "@components/community/question/CommentInput";
 import TgaList from "@components/community/TagList";
 import UserProfile from "@components/community/UserProfile";
 import { PATH } from "@constants/path";
@@ -14,7 +16,7 @@ export default function CommunityDefault() {
   }
 
   return (
-    <div className="cursor-pointer flex flex-col gap-2.5">
+    <div className="w-[1000px] mx-auto mt-14 cursor-pointer flex flex-col gap-4">
       <div className="flex justify-between items-center">
         <UserProfile nickname="한라봉" profileUrl="" />
         <button
@@ -25,23 +27,23 @@ export default function CommunityDefault() {
         </button>
       </div>
 
-      <div className="flex gap-4">
+      <div className="flex gap-8 mb-7">
         {/* TODO: || "https://placehold.co/490x320?text=CAMP+STORY" */}
-        <img src="/images/community/communityPostItem.png" alt="thumbnail" />
-
+        <div className="flex-shrink-0 w-[480px] h-[400px] rounded-lg overflow-hidden">
+          <img
+            src="/images/community/communityPostItem.png"
+            alt="thumbnail"
+            className="size-full object-cover"
+          />
+        </div>
         <div className="flex flex-col justify-between">
-          <div className="flex flex-col gap-[15px]">
-            <div className="flex gap-[15px] items-center text-[15px] text-gray-scale-500">
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-4 items-center text-[15px] text-gray-scale-500">
               <span>작성일</span>
               <span className="font-medium">2025.01.14</span>
             </div>
-
-            <div className="flex flex-col gap-2.5">
-              <TgaList tags={["clean", "kind", "convenience"]} />
-
-              <AreaCard location="충남 예산군" thumbnail="" title="스노우라인 캠핑빌리지" />
-            </div>
-
+            <TgaList tags={["clean", "kind", "convenience"]} />
+            <AreaCard location="충남 예산군" thumbnail="" title="스노우라인 캠핑빌리지" />
             <div className="text-[15px] text-gray-scale-400">
               안녕하세요. 이번엔 스노우라인 캠핑 빌리지에 다녀왔습니다. 정말 좋네요 추천합니다!
               다음엔 어디를 다녀올까요. 추천해주세요. 어디든 재밌게 갈 수 있습니다. 액티비티를
@@ -59,6 +61,22 @@ export default function CommunityDefault() {
             handleClickBookmark={() => alert("click bookmark")}
             handleClickLike={() => alert("click like")}
           />
+        </div>
+      </div>
+      <div className="flex flex-col gap-[30px]">
+        <div className="flex flex-col gap-4">
+          <div className="text-sub-title text-gray-scale-400 font-bold">댓글</div>
+          <CommentInput
+            handleSubmit={() => {
+              alert("submit!");
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-[40px]">
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
         </div>
       </div>
     </div>
--- src/pages/CommunityMain.tsx
+++ src/pages/CommunityMain.tsx
@@ -9,12 +9,12 @@ export default function CommunityMain() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="mt-[100px] mb-[60px]">
+      <div className="mt-14 mb-[60px]">
         <SearchInput handleSubmit={() => alert("submit!")} />
       </div>
 
       <div className="flex gap-[50px] justify-between items-center mb-[30px]">
-        <div className="flex gap-5 text-sm">
+        <div className="flex gap-5 text-sub-title">
           <OrderRadio label="최신순" value="recent" defaultChecked />
           <OrderRadio label="인기순" value="popular" />
         </div>
@@ -26,14 +26,16 @@ export default function CommunityMain() {
         </div>
 
         <button
-          className="px-3 py-2 ml-auto text-white text-sm bg-primary-500 rounded"
+          className="px-3 py-2 ml-auto text-white text-body1 bg-primary-500 rounded"
           onClick={() => navigate(PATH.communityCreate)}
         >
           글 작성하기
         </button>
       </div>
 
-      <div className="grid grid-cols-2 gap-x-[64px] gap-y-[60px]">
+      <div className="grid grid-cols-3 gap-x-8 gap-y-12">
+        <PostCard />
+        <PostCard />
         <PostCard />
         <PostCard />
         <PostCard />
