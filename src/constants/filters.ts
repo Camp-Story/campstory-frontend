fd0e61660a5919a48327f3e2bf8542052f86a911
leejihwan99 <lbwlimes@gmail.com>
2025-01-31 14:15:41 +0900

    feat: filters에서 code를 areaCode로 수정

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -80,57 +80,57 @@ export const RESTURANT_CATEGORY = [
 
 export const AREA = [
   {
-    code: 11,
+    areaCode: 1,
     value: "서울특별시",
     label: "서울",
   },
   {
-    code: 50,
+    areaCode: 39,
     value: "제주특별자치도",
     label: "제주",
   },
   {
-    code: 51,
+    areaCode: 32,
     value: "강원특별자치도",
     label: "강원도",
   },
   {
-    code: 26,
+    areaCode: 6,
     value: "부산광역시",
     label: "부산",
   },
   {
-    code: 46,
+    areaCode: 38,
     value: "전라남도",
     label: "전남",
   },
   {
-    code: 52,
+    areaCode: 37,
     value: "전북특별자치도",
     label: "전북",
   },
   {
-    code: 43,
+    areaCode: 33,
     value: "충청북도",
     label: "충북",
   },
   {
-    code: 44,
+    areaCode: 34,
     value: "충청남도",
     label: "충남",
   },
   {
-    code: 41,
+    areaCode: 31,
     value: "경기도",
     label: "경기도",
   },
   {
-    code: 47,
+    areaCode: 35,
     value: "경상북도",
     label: "경북",
   },
   {
-    code: 48,
+    areaCode: 36,
     value: "경상남도",
     label: "경남",
   },
