0bdb0d305f2aa0e8c89984c7bf19a2637af868b6
leejihwan99 <lbwlimes@gmail.com>
2025-01-31 14:14:49 +0900

    feat: 행사 검색 페이지 지역 기준 필터링 컴포넌트 추가

---
+++ src/components/event/EventAreaCheckbox.tsx
@@ -0,0 +1,35 @@
+// AreaCheckbox.tsx
+interface AreaCheckboxProps {
+  value: string;
+  code: number;
+  label: string;
+  selectedArea: number | null;
+  onAreaChange: (code: number) => void;
+}
+
+export default function EventAreaCheckbox({
+  code,
+  label,
+  value,
+  selectedArea,
+  onAreaChange,
+}: AreaCheckboxProps) {
+  return (
+    <div className="flex gap-2 items-center w-fit" key={code}>
+      <input
+        id={value}
+        type="checkbox"
+        value=""
+        className="hidden peer"
+        checked={selectedArea === code}
+        onChange={() => onAreaChange(code)}
+      />
+      <label
+        htmlFor={value}
+        className="cursor-pointer border border-gray-scale-100 hover:bg-gray-scale-100/50 peer-checked:border-[#1A9EFE]/50 peer-checked:bg-[#1A9EFE]/15 peer-checked:hover:bg-[#1A9EFE]/30 peer-checked:text-info-500 w-[70px] text-center py-1 rounded-[20px] text-[13px] font-bold"
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
