bc3f816a64b50ad10563c9b6358f8a23b976028f
Sean <joajoa70584@gmail.com>
2025-01-31 10:47:39 +0900

    style: update styles of navigation bar in header

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <div className="min-h-dvh">
       <div className="font-sans">
-        <header className="sticky top-0 w-full bg-gray-scale-0 px-main py-3 z-20">
+        <header className="sticky top-0 bg-gray-scale-0 py-3 z-20">
           <MainNavigation />
         </header>
         <Outlet />
