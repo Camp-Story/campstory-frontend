486fddd7883f8415b46f3e53c0a499e1d3940dad
leejihwan99 <lbwlimes@gmail.com>
2025-01-31 14:18:24 +0900

    feat: 행사 메인페이지 검색 기능 추가

--- src/components/event/SearchBar.tsx
+++ src/components/event/SearchBar.tsx
@@ -1,24 +1,47 @@
 import { PATH } from "@constants/path";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
-interface SearchBarProps {
-  className: string;
-}
+type SearchBarProps = {
+  className?: string;
+  backgroundColor?: string;
+};
 
-export default function SearchBar({ className }: SearchBarProps) {
+export default function SearchBar({ className, backgroundColor }: SearchBarProps) {
+  const [focus, setFocus] = useState(false);
+  const [query, setQuery] = useState<string>("");
   const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (query.trim()) {
+      navigate(`${PATH.eventSearch}?keyword=${encodeURIComponent(query)}`);
+    }
+  };
   return (
     <div className={className}>
-      <input
-        placeholder="검색어를 입력해주세요."
-        className="relative py-3 px-6 w-[892px] rounded-full placeholder-gray-scale-300 focus:outline-secondary-300"
-      />
-      <button
-        onClick={() => navigate(PATH.campingSearch)}
-        className="absolute top-[50%] -translate-y-[50%] right-[20px] p-2"
+      <form
+        onSubmit={handleSubmit}
+        className=" bg-slate-50 flex mx-auto items-center justify-between rounded-full relative"
       >
-        <img src="/SearchIcon.svg" alt="searchIcon" />
-      </button>
+        <input
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="검색어를 입력하세요"
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          className={`py-3 px-6 w-[800px] font-semibold rounded-full  ${
+            backgroundColor
+              ? focus
+                ? "bg-white placeholder-slate-600  focus:outline-green-700"
+                : "bg-slate-200 placeholder-slate-600  focus:outline-green-700"
+              : "bg-white focus:outline-secondary-300"
+          }`}
+        />
+        <button type="submit" className="absolute top-[50%] -translate-y-[50%] right-[20px] p-2">
+          <img src="/SearchIcon.svg" alt="searchIcon" />
+        </button>
+      </form>
     </div>
   );
 }
