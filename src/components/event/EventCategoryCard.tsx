6929ca165ddb934cc402e2b64ec935c3edda55b3
leejihwan99 <lbwlimes@gmail.com>
2025-01-31 09:44:04 +0900

    feat: 행사 메인 페이지 카테고리에서 검색 페이지에 반영

--- src/components/event/EventCategoryCard.tsx
+++ src/components/event/EventCategoryCard.tsx
@@ -5,12 +5,20 @@ interface EventCategoryCardProps {
   src: string;
   path: string;
   children: ReactNode;
+  keyword: string; // 추가된 키워드 props
 }
 
-export default function EventCategoryCard({ src, path, children }: EventCategoryCardProps) {
+export default function EventCategoryCard({
+  src,
+  path,
+  children,
+  keyword,
+}: EventCategoryCardProps) {
   const navigate = useNavigate();
   return (
-    <div onClick={() => navigate(path)}>
+    <div onClick={() => navigate(`${path}?keyword=${keyword}`)}>
+      {" "}
+      {/* URL에 키워드 추가 */}
       <img src={src} alt="Camping Image" className="hover:brightness-75" />
       <div className="text-[26px] text-gray-scale-400 font-bold my-4">{children}</div>
     </div>
