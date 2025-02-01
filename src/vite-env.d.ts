4fc3a8dc13ab85576ff72ed4ee741be415e47f33
Sean <joajoa70584@gmail.com>
2025-02-01 11:39:48 +0900

    chore: add weather API key type to vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMeta {
     VITE_API_BASE_URL: string;
     VITE_GO_CAMPING_API_KEY: string;
     VITE_TOUR_API_KEY: string;
+    VITE_WEATHER_API_KEY: string;
     VITE_NAVER_API_ID_KEY: string;
     VITE_NAVER_SECRET_KEY: string;
     VITE_KAKAO_API_KEY: string;
