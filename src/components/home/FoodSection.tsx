e54705c9ac04baa0fe3f24d05537b94ffe9d8810
Sean <joajoa70584@gmail.com>
2025-02-03 14:56:26 +0900

    feat: add category-based navigation logic to FoodSection

--- src/components/home/FoodSection.tsx
+++ src/components/home/FoodSection.tsx
@@ -1,31 +1,50 @@
 import { PATH } from "@constants/path";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
-const DUMMY_FOOD_CATEGORY: { src: string; category: string }[] = [
+interface FoodCategoryProps {
+  src: string;
+  category: string;
+  catId: string;
+}
+
+const FOOD_CATEGORY: FoodCategoryProps[] = [
   {
     src: "/images/main/main-food-korea.png",
     category: "한식",
+    catId: "A05020100",
   },
   {
     src: "/images/main/main-food-eng.png",
     category: "양식",
+    catId: "A05020200",
   },
   {
     src: "/images/main/main-food-china.png",
     category: "중식",
+    catId: "A05020400",
   },
   {
     src: "/images/main/main-food-japan.png",
     category: "일본",
+    catId: "A05020300",
   },
 ];
 
-function FoodCategoryItem({ src, category }: { src: string; category: string }) {
+function FoodCategoryItem({ src, category, catId }: FoodCategoryProps) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate({
+      pathname: PATH.restaurantSearch,
+      search: `search?cat3=${catId}`,
+    });
+  };
+
   return (
     <li className="col-span-2">
-      <Link to="/" className="block rounded-full overflow-hidden">
+      <div onClick={handleClick} className="block rounded-full overflow-hidden">
         <img src={src} alt="음식 카테고리 이미지" className="mb-2" />
-      </Link>
+      </div>
       <h4 className="text-center text-sub-title font-bold text-gray-scale-400">{category}</h4>
     </li>
   );
@@ -39,8 +58,13 @@ export default function FoodSection() {
         캠핑을 더욱 특별하게 만들어줄 음식을 만나보세요.
       </p>
       <ul className="grid grid-cols-4 gap-4">
-        {DUMMY_FOOD_CATEGORY.map((item) => (
-          <FoodCategoryItem key={item.category} src={item.src} category={item.category} />
+        {FOOD_CATEGORY.map((item) => (
+          <FoodCategoryItem
+            key={item.category}
+            src={item.src}
+            category={item.category}
+            catId={item.catId}
+          />
         ))}
       </ul>
       <Link to={PATH.restaurantSearch} className="absolute top-5 font-bold right-0 text-info-500">
