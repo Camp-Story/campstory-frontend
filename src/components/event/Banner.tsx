ecda366d8f5e97227920f8a4b8e52c6120590b3c
leejihwan99 <lbwlimes@gmail.com>
2025-02-03 14:26:38 +0900

    feat: 행사 메인 페이지 섹션들 마진값 수정

--- src/components/event/Banner.tsx
+++ src/components/event/Banner.tsx
@@ -2,7 +2,7 @@ import SearchBar from "./SearchBar";
 
 export default function Banner() {
   return (
-    <div className="relative">
+    <div className="relative mt-[10px]">
       <img
         src="/images/festival/festival-banner.png"
         alt="festival banner"
