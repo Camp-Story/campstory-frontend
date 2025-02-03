6495535f67fdfcdedce6a47001dda33160fa4d2a
leejihwan99 <lbwlimes@gmail.com>
2025-02-03 14:26:38 +0900

    feat: 행사 메인 페이지 섹션들 마진값 수정

--- src/components/event/SubTitle.tsx
+++ src/components/event/SubTitle.tsx
@@ -7,7 +7,7 @@ export default function SubTitle({ mainText, subText }: SubTitleProps) {
   return (
     <div className="mb-4">
       <div className="text-highlight text-gray-scale-400 font-impact">{mainText}</div>
-      <div className="mb-[20px] text-[20px] text-gray-scale-200">{subText}</div>
+      <div className="mb-[20px] text-[20px] text-gray-scale-300">{subText}</div>
     </div>
   );
 }
--- src/components/event/TodaySection.tsx
+++ src/components/event/TodaySection.tsx
@@ -10,15 +10,13 @@ export default function TodaySection({
   contentId,
 }: TodaySectionProps) {
   return (
-    <section className="flex mx-[105px] h-[400px]">
+    <section className="flex mx-[105px] h-[400px] my-9">
       <div className="flex-1 mr-[70px]">
-        <div className="text-highlight font-impact text-[52px] text-gray-scale-500 mb-[20px]">
+        <div className="font-impact text-[52px] leading-tight text-gray-scale-500 mb-4">
           {title}
         </div>
-        <div className="text-[20px] text-gray-scale-300 mb-14 max-h-[72px] overflow-y-auto">
-          {subTitle}
-        </div>
-        <div className="flex items-center text-[20px] text-gray-scale-300 mb-12">
+        <div className="text-[20px] leading-tight text-gray-scale-300 mb-11">{subTitle}</div>
+        <div className="flex items-center text-[20px] mb-12 text-gray-scale-300">
           {tag.map((t, index) => (
             <div key={index} className="flex items-center mr-4">
               <img
@@ -33,14 +31,14 @@ export default function TodaySection({
         <div className="pb-4">
           <Link
             to={`${path.replace(":id", contentId)}`}
-            className="border p-2 rounded-xl text-gray-scale-400 font-bold bg-gray-scale-100 w-[302px] h-[58px] flex justify-center items-center shadow-sm hover:bg-gray-300 transition"
+            className="border p-2 rounded-full text-[20px] text-gray-scale-400 font-bold bg-gray-scale-100 w-[302px] h-[58px] flex justify-center items-center shadow-sm hover:bg-gray-300 transition"
           >
             더 알아보기
           </Link>
         </div>
       </div>
       <div className="flex-1 flex justify-center">
-        <img src={src} alt="오늘의 행사 이미지" className="max-w-full" />
+        <img src={src} alt="오늘의 행사 이미지" className="max-w-[514px]" />
       </div>
     </section>
   );
