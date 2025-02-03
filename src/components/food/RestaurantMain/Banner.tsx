77d584148751f19fec17e447baab0640922236da
leejihwan99 <lbwlimes@gmail.com>
2025-02-03 17:14:48 +0900

    feat: 음식 메인 페이지 배너 마진값 추가

--- src/components/food/RestaurantMain/Banner.tsx
+++ src/components/food/RestaurantMain/Banner.tsx
@@ -2,7 +2,7 @@ import SearchBar from "./SearchBar";
 
 export default function Banner() {
   return (
-    <div className="relative">
+    <div className="relative mt-[10px]">
       <img src="/images/food/food-banner.png" alt="메인이미지" className="w-full object-cover" />
       <SearchBar className="absolute bottom-[60px] left-[50%] -translate-x-[50%]" />
     </div>
