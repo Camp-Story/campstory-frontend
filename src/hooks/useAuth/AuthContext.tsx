ae0ac705fadd776e0db3c2ca149b18d9fb4f2fca
soyoung125 <sypark041754@gmail.com>
2025-02-03 11:47:02 +0900

    feat: update user info

--- src/hooks/useAuth/AuthContext.tsx
+++ src/hooks/useAuth/AuthContext.tsx
@@ -1,11 +1,13 @@
 import { User } from "types/AuthResponse";
 import { createContext } from "react";
+import { UserInfoState } from "@pages/MypageInfo";
 
 interface AuthContextType {
   user: User | null;
   login: (userData: User) => void;
   logout: () => void;
   updateUser: () => void;
+  modifyUser: (userData: UserInfoState) => void;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
--- src/hooks/useAuth/AuthProvider.tsx
+++ src/hooks/useAuth/AuthProvider.tsx
@@ -2,6 +2,7 @@ import { User } from "types/AuthResponse";
 import { useState, ReactNode } from "react";
 import { apiInstance } from "@utils/axiosInstance";
 import AuthContext from "./AuthContext";
+import { UserInfoState } from "@pages/MypageInfo";
 
 export default function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
@@ -25,8 +26,27 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
     setUser(response.data);
   };
 
+  const modifyUser = async (userData: UserInfoState) => {
+    const token = localStorage.getItem("token");
+
+    const response = await apiInstance.put<User>(
+      "/settings/update-user",
+      {
+        fullName: JSON.stringify(userData),
+        username: userData.nickName,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    setUser(response.data);
+  };
+
   return (
-    <AuthContext.Provider value={{ user, login, logout, updateUser }}>
+    <AuthContext.Provider value={{ user, login, logout, updateUser, modifyUser }}>
       {children}
     </AuthContext.Provider>
   );
