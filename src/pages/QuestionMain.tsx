543ba85de4ea5cd40d23f35f1d34c96bcf789b30
marie <marie061313@gmail.com>
2025-02-04 16:20:04 +0900

    feat: 질문 검색 기능 구현

--- src/pages/QuestionMain.tsx
+++ src/pages/QuestionMain.tsx
@@ -4,16 +4,37 @@ import QuestionCard from "@components/community/question/QuestionCard";
 import QuestionTag from "@components/community/QuestionTag";
 import { QUESTION_CHANNEL_ID } from "@constants/channelId";
 import { PATH } from "@constants/path";
-import { apiInstance } from "@utils/axiosInstance";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
 import PostResponse from "types/PostResponse";
+import { apiInstance } from "@utils/axiosInstance";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
+// import PostResponse from "types/PostResponse";
 
 export default function QuestionMain() {
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [keyword, setKeyword] = useState<string>();
   const [questionData, setQuestionData] = useState<PostResponse[]>([]);
+  const [filteredData, setFilteredData] = useState<PostResponse[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+
+  const handleSearch = (input: string) => {
+    searchParams.set("keyword", input);
+    setSearchParams(searchParams);
+    setKeyword(input);
+  };
+
+  const searchQuestionData = useCallback(
+    (input: string) => {
+      const data = questionData.filter((question) =>
+        JSON.parse(question.title).title.includes(input),
+      );
+      console.log("filteredData", data);
+      setFilteredData(data);
+    },
+    [questionData],
+  );
 
   const fetchQuestionData = async () => {
     setIsLoading(true);
@@ -23,6 +44,7 @@ export default function QuestionMain() {
         `/posts/channel/${QUESTION_CHANNEL_ID}`,
       );
       setQuestionData(response.data);
+      setFilteredData(response.data);
       console.log(response.data);
       console.log(JSON.parse(response.data[0].title).title);
     } catch (error) {
@@ -36,11 +58,19 @@ export default function QuestionMain() {
   useEffect(() => {
     fetchQuestionData();
   }, []);
+
+  useEffect(() => {
+    if (keyword) {
+      searchQuestionData(keyword);
+    }
+  }, [keyword, searchQuestionData]);
+
   return (
     <div className="mt-14 px-4">
       {/* SearchBar */}
       <div className="mb-[60px]">
-        <SearchInput handleSubmit={() => alert("submit!")} />
+        <SearchInput handleSubmit={(input) => handleSearch(input)} />
+        {/* <SearchInput handleSubmit={(input) => <>{input}</>} /> */}
       </div>
 
       <div className="flex gap-[50px] justify-between items-center mb-[30px]">
@@ -73,14 +103,20 @@ export default function QuestionMain() {
       {isLoading && <p>로딩중...</p>}
       {error}
       <div className="grid grid-cols-2 gap-x-7 gap-y-10 justify-between">
-        {questionData.map((question) => (
+        {filteredData.map((question) => (
           <QuestionCard
             key={question._id}
-            handleClick={() =>
-              navigate(PATH.questionPost(question._id), { state: { userId: question.author._id } })
+            handleClick={
+              () =>
+                navigate(PATH.questionPost(question._id), {
+                  state: { userId: question.author._id },
+                })
+              // navigate(PATH.questionPost(question._id), { state: { userId: question.author } })
             }
             userName={JSON.parse(question.author.fullName).fullName}
+            // userName={"한라봉"}
             coverImage={question.author.image || "https://placehold.co/30x30?text=CAMP+STORY"}
+            // coverImage={"https://placehold.co/30x30?text=CAMP+STORY"}
             title={JSON.parse(question.title).title}
             timeStamp={new Date(question.createdAt).toLocaleDateString()}
           />
