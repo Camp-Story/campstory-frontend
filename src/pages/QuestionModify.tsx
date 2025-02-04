cd4235946d2cb86cf419a74f1213de3d0c5f79ed
marie <marie061313@gmail.com>
2025-02-04 10:35:10 +0900

    feat: 질문 수정 기능 구현

--- src/pages/QuestionModify.tsx
+++ src/pages/QuestionModify.tsx
@@ -1,32 +1,116 @@
 import InputContainer from "@components/community/question/QuestionCreate/InputContainer";
 import QuestionTag from "@components/community/QuestionTag";
+import PostResponse from "types/PostResponse";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { apiInstance } from "@utils/axiosInstance";
+import { QUESTION_CHANNEL_ID } from "@constants/channelId";
+import { PATH } from "@constants/path";
 
 export default function QuestionModify() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  const [questionData, setQuestionData] = useState<PostResponse>();
+  const [formData, setFormData] = useState({ title: "", tag: [] as string[], content: "" });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchDetailData = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await apiInstance.get<PostResponse>(`/posts/${id}`);
+      setQuestionData(response.data);
+    } catch (error) {
+      setError("질문 정보를 가져오는 중 오류가 발생했습니다.");
+      console.log("Error fetching question data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [id]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      tag: checked ? [...prev.tag, value] : prev.tag.filter((tag) => tag !== value),
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await apiInstance.put(
+      "/posts/update",
+      {
+        postId: id,
+        title: JSON.stringify(formData),
+        channelId: QUESTION_CHANNEL_ID,
+        image: null,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+    if (response.status === 200 || response.status === 201) {
+      alert("수정 성공!");
+      navigate(PATH.questionPost(id!));
+    }
+  };
+
+  useEffect(() => {
+    fetchDetailData();
+  }, [fetchDetailData]);
+
+  useEffect(() => {
+    if (questionData) {
+      try {
+        const { title, tag, content } = JSON.parse(questionData.title);
+        setFormData({ title, tag, content });
+      } catch (error) {
+        console.error("JSON 파싱 에러:", error);
+      }
+    }
+  }, [questionData]);
+
   return (
     <div className="w-[618px] mx-auto mt-[52px]">
       <h1 className="mb-[30px] text-[26px] font-bold">질문 수정하기</h1>
-
-      <div className="flex flex-col gap-5">
+      {isLoading && <p>로딩중...</p>}
+      {error}
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <InputContainer title="제목">
           <input
+            name="title"
             placeholder="이곳에 제목을 입력해주세요 (최대 20자 작성)"
             maxLength={20}
+            onChange={handleChange}
+            defaultValue={formData.title}
             className="p-3.5 px-[25px] text-[15px] border border-gray-scale-200 rounded-sm focus:outline-none"
           />
         </InputContainer>
         <InputContainer title="카테고리">
           <div className="flex gap-2">
-            <QuestionTag tag="reservation" isCheckbox />
-            <QuestionTag tag="payment" isCheckbox />
-            <QuestionTag tag="member" isCheckbox />
-            <QuestionTag tag="campingGear" isCheckbox />
-            <QuestionTag tag="campsite" isCheckbox />
-            <QuestionTag tag="tips" isCheckbox />
+            <QuestionTag tag="reservation" isCheckbox handleChange={handleCheckbox} />
+            <QuestionTag tag="payment" isCheckbox handleChange={handleCheckbox} />
+            <QuestionTag tag="member" isCheckbox handleChange={handleCheckbox} />
+            <QuestionTag tag="campingGear" isCheckbox handleChange={handleCheckbox} />
+            <QuestionTag tag="campsite" isCheckbox handleChange={handleCheckbox} />
+            <QuestionTag tag="tips" isCheckbox handleChange={handleCheckbox} />
           </div>
         </InputContainer>
         <InputContainer title="내용">
           <textarea
             autoFocus
+            name="content"
+            onChange={handleChange}
+            defaultValue={formData.content}
             className="h-[466px] p-[25px] border border-gray-scale-200 rounded-sm focus:outline-none text-[15px]"
             placeholder="이곳에 내용을 작성해주세요 (최소 10자 이상 작성)"
           />
@@ -34,7 +118,7 @@ export default function QuestionModify() {
         <button className="rounded bg-primary-500 text-white text-sub-title py-3 w-full text-center mt-[5px]">
           수정하기
         </button>
-      </div>
+      </form>
     </div>
   );
 }
