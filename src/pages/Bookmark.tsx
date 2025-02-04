b8650bf460377d182e97623b2ef3b8f1bac4f4c5
soyoung125 <sypark041754@gmail.com>
2025-02-04 15:34:51 +0900

    refactor: add empty bookmark list info

--- src/pages/Bookmark.tsx
+++ src/pages/Bookmark.tsx
@@ -2,18 +2,32 @@ import BookmarkCard from "@components/mypage/BookmarkCard";
 import { PATH } from "@constants/path";
 import useBookMark from "@hooks/useBookmark";
 import useSearchAndNavigateCamping from "@hooks/useSearchAndNavigateCamping";
+import { useNavigate } from "react-router";
 
 export default function Bookmark() {
   const { likes, posts, handleUnlike } = useBookMark("67a0d8576e0e9a207c06c4ee");
   const { searchAndNavigate } = useSearchAndNavigateCamping();
+  const navigate = useNavigate();
+
+  const likedPosts = posts.filter((post) => likes.find((like) => like.post === post._id));
 
   return (
     <div>
       <div className="text-[26px] font-bold mb-8">찜 목록</div>
+      {likedPosts.length === 0 && (
+        <h2 className="text-body1">
+          찜한 캠핑 장소가 없습니다.{" "}
+          <span
+            className="text-primary-500 hover:cursor-pointer"
+            onClick={() => navigate(PATH.campingSearch)}
+          >
+            추가하기
+          </span>
+        </h2>
+      )}
       <div className="grid grid-cols-2 gap-x-11 gap-y-[30px] mb-10">
-        {likes.map((like) => {
-          const post = posts.find((p) => p._id === like.post);
-          const { title, category, image, location, id } = JSON.parse(post?.title || "{}");
+        {likedPosts.map((post) => {
+          const { title, category, image, location, id } = JSON.parse(post.title || "{}");
           return (
             post && (
               <BookmarkCard
