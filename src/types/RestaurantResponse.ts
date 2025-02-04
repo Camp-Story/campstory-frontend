a2bb0b9c9e79b0b089f7271f830dbf1125798492
Sean <joajoa70584@gmail.com>
2025-02-04 12:08:07 +0900

    feat: add RestaurantResponse interface

---
+++ src/types/RestaurantResponse.ts
@@ -0,0 +1,21 @@
+interface RestaurantDetailData {
+  contentid: string;
+  originimgurl: string;
+  title: string;
+  tel: string;
+  cat3: string;
+  zipcode: string;
+  mapx: string;
+  mapy: string;
+  overview: string;
+  homepage: string;
+  addr1: string;
+  addr2: string;
+  opentimefood: string;
+  parkingfood: string;
+  firstmenu: string;
+  infocenterfood: string;
+  restdatefood: string;
+}
+
+export type { RestaurantDetailData };
