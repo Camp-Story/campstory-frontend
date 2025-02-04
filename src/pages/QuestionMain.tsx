0ff14856dbc3a07f5694be8b014ea8cdee1d505d
marie <marie061313@gmail.com>
2025-02-04 17:07:08 +0900

    feat: 질문 필터 기능 구현

--- src/pages/QuestionMain.tsx
+++ src/pages/QuestionMain.tsx
@@ -1,23 +1,25 @@
 import SearchInput from "@components/common/SearchInput";
 import OrderRadio from "@components/community/OrderRadio";
 import QuestionCard from "@components/community/question/QuestionCard";
-import QuestionTag from "@components/community/QuestionTag";
+import QuestionTag, { Tag } from "@components/community/QuestionTag";
 import { QUESTION_CHANNEL_ID } from "@constants/channelId";
 import { PATH } from "@constants/path";
 import PostResponse from "types/PostResponse";
 import { apiInstance } from "@utils/axiosInstance";
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router";
-// import PostResponse from "types/PostResponse";
+
+const TAGS: Tag[] = ["reservation", "payment", "member", "campingGear", "campsite", "tips"];
 
 export default function QuestionMain() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const [keyword, setKeyword] = useState<string>();
   const [questionData, setQuestionData] = useState<PostResponse[]>([]);
   const [filteredData, setFilteredData] = useState<PostResponse[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [keyword, setKeyword] = useState<string>();
+  const [tagList, setTagList] = useState<string[]>([]);
 
   const handleSearch = (input: string) => {
     searchParams.set("keyword", input);
@@ -25,12 +27,34 @@ export default function QuestionMain() {
     setKeyword(input);
   };
 
-  const searchQuestionData = useCallback(
-    (input: string) => {
-      const data = questionData.filter((question) =>
-        JSON.parse(question.title).title.includes(input),
-      );
-      console.log("filteredData", data);
+  const handleCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const tags = [...tagList];
+    if (e.target.checked) {
+      console.log(e.target.value);
+      tags.push(e.target.value);
+    } else {
+      const index = tags.indexOf(e.target.value);
+      if (index !== -1) {
+        tags.splice(index, 1);
+      }
+    }
+    searchParams.set("tags", tags.join(","));
+    setSearchParams(searchParams);
+    setTagList(tags);
+  };
+
+  const filterQuestionData = useCallback(
+    (input: string | undefined, tags: string[]) => {
+      let data = questionData;
+      if (input) {
+        data = data.filter((question) => JSON.parse(question.title).title.includes(input));
+        console.log("filteredData", data);
+      }
+      if (tags.length !== 0) {
+        data = data.filter((question) =>
+          tags.some((tag) => JSON.parse(question.title).tag.includes(tag)),
+        );
+      }
       setFilteredData(data);
     },
     [questionData],
@@ -60,17 +84,16 @@ export default function QuestionMain() {
   }, []);
 
   useEffect(() => {
-    if (keyword) {
-      searchQuestionData(keyword);
+    if (keyword || tagList) {
+      filterQuestionData(keyword, tagList);
     }
-  }, [keyword, searchQuestionData]);
+  }, [keyword, filterQuestionData, tagList]);
 
   return (
     <div className="mt-14 px-4">
       {/* SearchBar */}
       <div className="mb-[60px]">
         <SearchInput handleSubmit={(input) => handleSearch(input)} />
-        {/* <SearchInput handleSubmit={(input) => <>{input}</>} /> */}
       </div>
 
       <div className="flex gap-[50px] justify-between items-center mb-[30px]">
@@ -82,12 +105,15 @@ export default function QuestionMain() {
 
         {/* Tags */}
         <div className="flex gap-3">
-          <QuestionTag tag="reservation" isCheckbox />
-          <QuestionTag tag="payment" isCheckbox />
-          <QuestionTag tag="member" isCheckbox />
-          <QuestionTag tag="campingGear" isCheckbox />
-          <QuestionTag tag="campsite" isCheckbox />
-          <QuestionTag tag="tips" isCheckbox />
+          {TAGS.map((tag) => (
+            <QuestionTag
+              tag={tag}
+              isCheckbox
+              handleChange={(e) => {
+                handleCheckbox(e);
+              }}
+            />
+          ))}
         </div>
 
         {/* Button */}
@@ -106,17 +132,13 @@ export default function QuestionMain() {
         {filteredData.map((question) => (
           <QuestionCard
             key={question._id}
-            handleClick={
-              () =>
-                navigate(PATH.questionPost(question._id), {
-                  state: { userId: question.author._id },
-                })
-              // navigate(PATH.questionPost(question._id), { state: { userId: question.author } })
+            handleClick={() =>
+              navigate(PATH.questionPost(question._id), {
+                state: { userId: question.author._id },
+              })
             }
             userName={JSON.parse(question.author.fullName).fullName}
-            // userName={"한라봉"}
             coverImage={question.author.image || "https://placehold.co/30x30?text=CAMP+STORY"}
-            // coverImage={"https://placehold.co/30x30?text=CAMP+STORY"}
             title={JSON.parse(question.title).title}
             timeStamp={new Date(question.createdAt).toLocaleDateString()}
           />
