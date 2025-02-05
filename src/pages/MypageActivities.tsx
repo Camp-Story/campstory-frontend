9dc51c41213e3ef8affb79d3a8d79d077de3d592
marie <marie061313@gmail.com>
2025-02-05 15:00:27 +0900

    feat: 커뮤니티 데이터 연동

--- src/pages/MypageActivities.tsx
+++ src/pages/MypageActivities.tsx
@@ -1,17 +1,56 @@
 import MyPost from "@components/mypage/MyPost";
 import MyQuestion from "@components/mypage/MyQuestion";
 import MyComment from "@components/mypage/MyComment";
+import { apiInstance } from "@utils/axiosInstance";
+import { useCallback, useEffect, useState } from "react";
+import PostResponse from "types/PostResponse";
+
+const COMMUNITY_CHANNEL_ID = "67a021790b62dc0dc6cc8e69";
+const QUESTION_CHANNEL_ID = "67a01c3c896e1c0cc883e18c";
 
 export default function MypageActivities() {
+  const [posts, setPosts] = useState<PostResponse[]>([]);
+  const [questions, setQuestions] = useState<PostResponse[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const userid = localStorage.getItem("id");
+
+  const fetchPostData = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await apiInstance.get<PostResponse[]>(`/posts/author/${userid}`);
+      const myPosts = response.data.filter((post) => post.channel._id === COMMUNITY_CHANNEL_ID);
+      const myQuestions = response.data.filter((post) => post.channel._id === QUESTION_CHANNEL_ID);
+      setPosts(myPosts);
+      setQuestions(myQuestions);
+      console.log(questions);
+    } catch (error) {
+      setError("커뮤니티 포스트를 불러오는 중 오류가 발생했습니다.");
+      console.log("Error fetching camping data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [userid]);
+
+  useEffect(() => {
+    fetchPostData();
+  }, [fetchPostData]);
   return (
     <div className="max-w-[880px]">
+      {isLoading && <p>로딩중...</p>}
+      {error}
       <h2 className="text-title font-bold">나의 활동</h2>
 
       <h3 className="text-sub-title mt-8">내 게시물</h3>
       <div className="flex flex-shrink-0 flex-wrap justify-between mt-4 gap-y-10">
-        <MyPost />
-        <MyPost />
-        <MyPost />
+        {posts.map((post) => (
+          <MyPost
+            content={JSON.parse(post.title).content}
+            tags={JSON.parse(post.title).tags}
+            image={post.image!}
+          />
+        ))}
       </div>
 
       <h3 className="text-sub-title mt-8">내 질문</h3>
