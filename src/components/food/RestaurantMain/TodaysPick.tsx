9a7009901aa3e159afcb46a48ff40167a9cd34d0
marie <marie061313@gmail.com>
2025-02-05 16:46:30 +0900

    design: 글자 색상 변경

--- src/components/food/RestaurantMain/TodaysPick.tsx
+++ src/components/food/RestaurantMain/TodaysPick.tsx
@@ -13,7 +13,9 @@ export default function TodaysPick({ src, title, summary, path }: TodaysPickProp
   return (
     <div className="rounded-lg hover:brightness-75" onClick={() => navigate(path)}>
       <img src={src} alt="tofu-restaurant" />
-      <span className="text-[26px] font-bold text-left inline-block mt-[12px]">{title}</span>
+      <span className="text-[26px] text-gray-scale-400 font-bold text-left inline-block mt-[12px]">
+        {title}
+      </span>
       <p className="text-gray-scale-200 text-[15px]">{summary}</p>
     </div>
   );
