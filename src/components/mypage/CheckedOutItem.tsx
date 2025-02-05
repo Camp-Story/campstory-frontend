8d6fdca1b0d11d45f086bf1ea85211f234a51b17
Sean <joajoa70584@gmail.com>
2025-02-05 14:58:10 +0900

    feat: add reservation history view for before and after use

---
+++ src/components/mypage/CheckedOutItem.tsx
@@ -0,0 +1,31 @@
+interface CheckedOutItemProps {
+  title: string;
+  location: string;
+  image: string;
+}
+
+export default function CheckedOutItem({ title, location, image }: CheckedOutItemProps) {
+  return (
+    <li className="flex gap-6">
+      <div className="size-[150px] rounded-xl overflow-hidden">
+        <img
+          className="size-full grayscale object-cover"
+          src={image || "https://placehold.co/200x200?text=CAMP+STORY"}
+          alt="thumbnail"
+        />
+      </div>
+      <div className="flex flex-col justify-between">
+        <div>
+          <div className="inline rounded-full px-3 py-1 text-body1 bg-secondary-300/20">
+            이용완료
+          </div>
+          <div className="mt-2 text-body1 mb-1">{title || "장소명이 없습니다."}</div>
+          <div className="text-body2 text-gray-scale-400">{location || "주소가 없습니다."}</div>
+        </div>
+        <div className="text-body2 bg-gray-200 w-20 py-1 flex items-center justify-center rounded-sm">
+          다시예약
+        </div>
+      </div>
+    </li>
+  );
+}
--- src/pages/ReservedList.tsx
+++ src/pages/ReservedList.tsx
@@ -1,113 +1,77 @@
+import { Post } from "types/ChannelResponse";
+import { apiInstance } from "@utils/axiosInstance";
+import { useEffect, useState } from "react";
+import ReservedItem from "@components/mypage/ReservedItem";
+import CheckedOutItem from "@components/mypage/CheckedOutItem";
+
 export default function ReservedList() {
+  const id = localStorage.getItem("id");
+  const [myReservations, setMyReservations] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await apiInstance.get<Post[]>(`/posts/channel/67a0223e0b62dc0dc6cc8e6d`);
+        setMyReservations(res.data.filter((post) => post.author._id === id));
+      } catch (error) {
+        console.error("Failed to fetch posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, [id]);
+
+  const currentDate = new Date();
+  const upcomingReservations =
+    myReservations?.length !== 0
+      ? myReservations?.filter((item) => {
+          let reservationDetail;
+          if (item.title) {
+            reservationDetail = JSON.parse(item.title || "{}");
+          }
+          const reservationDate = new Date(reservationDetail.date);
+          return reservationDate > currentDate;
+        })
+      : [];
+
+  const pastReservations =
+    myReservations?.length !== 0
+      ? myReservations?.filter((item) => {
+          let reservationDetail;
+          if (item.title) {
+            reservationDetail = JSON.parse(item.title || "{}");
+          }
+          const reservationDate = new Date(reservationDetail.date);
+          return reservationDate <= currentDate;
+        })
+      : [];
+
   return (
     <>
-      <div className="">
-        <div className="text-[26px] font-bold">예약 내역</div>
-        <div className=" mt-[25px] flex flex-col">
-          <div className="text-[20px] font-bold">이용 전</div>
-          <div className="flex">
-            <div className="size-[250px] shrink-0 rounded-l-lg">
-              <img
-                className="object-cover"
-                src="https://placehold.co/250x250?text=CAMP+STORY"
-                alt="thumbnail"
-              />
-            </div>
-            <div className="ml-[38px] mt-[20px]">
-              <div className="text-[13px] text-bold">예약확정</div>
-              <div className="text-[13px] text-bold text-primary-500">D-7</div>
-              <div className="text-gray-400 border-t-2 w-72  mt-[13px]"></div>
-              <div className="text-[20px] text-bold  mt-[15px]">아로니움 글램핑</div>
-              <div className="text-[13px] text-bold">인천 강화군 삼산면</div>
-              <div className="flex mt-[15px] divide-x-2 gap-9">
-                <div className="text-gray-500 text-[13px]">
-                  체크인
-                  <div className="text-black">01.15(수) 15:00 </div>
-                </div>
-                <div className="text-gray-500 text-[13px]">
-                  체크아웃
-                  <div className="text-black">01.16(수) 11:00 </div>
-                </div>
-              </div>
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[18px] flex items-center justify-center rounded-sm">
-                예약 상세
-              </div>
-            </div>
-          </div>
+      <div className="mb-36">
+        <div className="text-title font-bold mb-4">예약 내역</div>
+        <div className="flex flex-col mb-10">
+          <div className="text-sub-title font-bold mb-3">이용 전</div>
+          <ul className="flex flex-col gap-6">
+            {upcomingReservations.map((item) => {
+              if (item.title) {
+                const reservationDetail = JSON.parse(item.title || "{}");
+                return <ReservedItem key={reservationDetail.date} {...reservationDetail} />;
+              }
+            })}
+          </ul>
         </div>
 
-        <div className="text-[20px] font-bold mt-6">이용 완료 및 예약취소</div>
-        <div className="flex mt-[49px] ">
-          <div className="size-[150px] bg-gray-300 flex items-center justify-center rounded-md"></div>
-          <div className="ml-[20px]">
-            <div className="text-gray-500 text-[13px]">이용완료</div>
-            <div className="text-bold text-[13px] mt-[10px]">아로니움 글램핑</div>
-            <div className="text-gray-600 text-[13px] mt-[7px]">인천 강화군 삼산면</div>
-            <div className="flex gap-2">
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                다시예약
-              </div>
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                예약 상세
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border-t-2 w-[620px] mt-3"></div>
-        <div className="flex mt-[49px] ">
-          <div className="size-[150px] bg-gray-300 flex items-center justify-center rounded-md"></div>
-          <div className="ml-[20px]">
-            <div className="text-gray-500 text-[13px]">이용완료</div>
-            <div className="text-bold text-[13px] mt-[10px]">아로니움 글램핑</div>
-            <div className="text-gray-600 text-[13px] mt-[7px]">인천 강화군 삼산면</div>
-            <div className="flex gap-2">
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                다시예약
-              </div>
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                예약 상세
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border-t-2 w-[620px] mt-3"></div>
-        <div className="flex mt-[49px] ">
-          <div className="size-[150px] bg-gray-300 flex items-center justify-center rounded-md"></div>
-          <div className="ml-[20px]">
-            <div className="text-gray-500 text-[13px]">이용완료</div>
-            <div className="text-bold text-[13px] mt-[10px]">아로니움 글램핑</div>
-            <div className="text-gray-600 text-[13px] mt-[7px]">인천 강화군 삼산면</div>
-            <div className="flex gap-2">
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                다시예약
-              </div>
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                예약 상세
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border-t-2 w-[620px] mt-3"></div>
-        <div className="flex mt-[49px] ">
-          <div className="size-[150px] bg-gray-300 flex items-center justify-center rounded-md"></div>
-          <div className="ml-[20px]">
-            <div className="text-gray-500 text-[13px]">이용완료</div>
-            <div className="text-bold text-[13px] mt-[10px]">아로니움 글램핑</div>
-            <div className="text-gray-600 text-[13px] mt-[7px]">인천 강화군 삼산면</div>
-            <div className="flex gap-2">
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                다시예약
-              </div>
-              <div className="text-[13px] bg-gray-300 w-[107px] h-[33px] mt-[43px] flex items-center justify-center rounded-sm">
-                예약 상세
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border-t-2 w-[620px] mt-3"></div>
+        <div className="text-sub-title font-bold mb-3">이용 완료 및 예약취소</div>
+        <ul className="flex flex-col gap-6">
+          {pastReservations.map((item) => {
+            if (item.title) {
+              const reservationDetail = JSON.parse(item.title || "{}");
+              return <CheckedOutItem key={reservationDetail.date} {...reservationDetail} />;
+            }
+          })}
+        </ul>
       </div>
-      <br /> <br /> <br /> <br /> <br />
-      <br /> <br /> <br /> <br /> <br />
     </>
   );
 }
