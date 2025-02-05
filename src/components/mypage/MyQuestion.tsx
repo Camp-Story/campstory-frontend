0da5b6306bdeaa6520eb7f659f49fabcac12935e
marie <marie061313@gmail.com>
2025-02-05 15:03:15 +0900

    feat: MyQuestion 컴포넌트 props 수정

--- src/components/mypage/MyQuestion.tsx
+++ src/components/mypage/MyQuestion.tsx
@@ -1,7 +1,11 @@
 import { useNavigate } from "react-router";
 import { PATH } from "@constants/path";
 
-export default function MyQuestion() {
+interface QuestionProps {
+  title: string;
+}
+
+export default function MyQuestion({ title }: QuestionProps) {
   const navigate = useNavigate();
 
   return (
@@ -11,9 +15,7 @@ export default function MyQuestion() {
         <div className="flex flex-col gap-4 bg-gray-scale-0 w-full p-[20px] rounded-lg drop-shadow-custom">
           <div className="flex gap-4 items-center">
             <div className="text-primary-500 font-bold text-[26px]">Q.</div>
-            <div className="text-body1 text-gray-scale-400">
-              안녕하세요~ 보통 캠핑장 매점에서 구매하는 주류는 가격이 어떻게 될까용?
-            </div>
+            <div className="text-body1 text-gray-scale-400">{title}</div>
           </div>
         </div>
       </div>
