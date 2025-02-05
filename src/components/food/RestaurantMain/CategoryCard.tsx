771e6076c47bc2d346b729f9713e58b72753341b
marie <marie061313@gmail.com>
2025-02-05 16:46:59 +0900

    design: 카테고리 카드 디자인 수정

--- src/components/food/RestaurantMain/CategoryCard.tsx
+++ src/components/food/RestaurantMain/CategoryCard.tsx
@@ -9,14 +9,14 @@ export default function CategoryCard({ src, catName, onClick }: CategoryCardProp
     <>
       <button
         onClick={onClick}
-        className="flex flex-col items-center justify-center h-[196px] mt-7 bg-white shadow-md rounded-lg hover:brightness-75 transition"
+        className="flex flex-col items-center justify-center py-[40px] px-[30px] bg-white drop-shadow-custom rounded-xl hover:shadow-lg transition"
       >
         <div className="w-15 bg-gray-200 rounded-full">
           {/* 아이콘/이미지 */}
           <img src={src} alt="flags" className="m-3" />
         </div>
         {/* 카테고리 이름 */}
-        <span className="text-[20px] font-semibold mt-3">{catName}</span>
+        <span className="text-sub-title text-gray-scale-400 font-semibold mt-3">{catName}</span>
       </button>
     </>
   );
