7eccf7387bb5373946678d18307b8e507bd335a1
marie <marie061313@gmail.com>
2025-02-05 12:34:29 +0900

    feat: 커뮤니티 검색 및 필터 기능 추가

--- src/pages/CommunityMain.tsx
+++ src/pages/CommunityMain.tsx
@@ -3,8 +3,8 @@ import OrderRadio from "@components/community/OrderRadio";
 import PostCard from "@components/community/community/PostCard";
 import Tag from "@components/community/Tag";
 import { PATH } from "@constants/path";
-import { useNavigate } from "react-router";
-import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
+import { useCallback, useEffect, useState } from "react";
 import { apiInstance } from "@utils/axiosInstance";
 import getRelativeTime from "@utils/getRelativeTime";
 import { COMMUNITY_CHANNEL_ID } from "@constants/channelId";
@@ -25,11 +25,62 @@ interface Post {
   author: Author;
 }
 
+const KEYWORD_PARAM = "keyword";
+const TAG_PARAM = "tag";
+
 export default function CommunityMain() {
   const navigate = useNavigate();
-
+  const [searchParams, setSearchParams] = useSearchParams();
   const [posts, setPosts] = useState<Post[]>([]);
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
   const [sortOrder, setSortOrder] = useState<"recent" | "popular">("recent");
+  const keyword = searchParams.get(KEYWORD_PARAM) || null;
+  const [tagList, setTagList] = useState<string[]>(
+    searchParams.get("tags") ? searchParams.get("tags")!.split(",") : [],
+  );
+
+  const updateSearchParams = useCallback(
+    (key: string, value: string) => {
+      const newParams = new URLSearchParams(searchParams);
+      newParams.set(key, value);
+      setSearchParams(newParams);
+    },
+    [searchParams, setSearchParams],
+  );
+
+  const handleTagChange = (tag: string, checked: boolean) => {
+    const tags = [...tagList];
+    if (checked) tags.push(tag);
+    else {
+      const index = tagList.indexOf(tag);
+      if (index !== -1) {
+        tags.splice(index, 1);
+      }
+    }
+    updateSearchParams(TAG_PARAM, tags.join(","));
+    setTagList(tags);
+  };
+
+  const handleSearch = useCallback(
+    (searchKeyword: string) => {
+      updateSearchParams(KEYWORD_PARAM, searchKeyword);
+    },
+    [updateSearchParams],
+  );
+
+  const filterPostList = useCallback(
+    (input: string | null, tags: string[]) => {
+      let data = posts;
+      if (input) {
+        data = data.filter((post) => JSON.parse(post.title).content.includes(input));
+      }
+      if (tags.length !== 0) {
+        data = data.filter((post) => tags.some((tag) => JSON.parse(post.title).tags.includes(tag)));
+      }
+      setFilteredPosts(data);
+    },
+    [posts],
+  );
 
   useEffect(() => {
     apiInstance
@@ -37,13 +88,20 @@ export default function CommunityMain() {
       .then((res) => {
         const validPosts = res.data.filter((post) => post.author);
         setPosts(validPosts);
+        setFilteredPosts(validPosts);
       })
       .catch((err) => {
         console.error("게시글 불러오기 실패:", err);
       });
   }, []);
 
-  const sortedPosts = [...posts].sort((a, b) => {
+  useEffect(() => {
+    if (keyword || tagList) {
+      filterPostList(keyword, tagList);
+    }
+  }, [keyword, filterPostList, tagList]);
+
+  const sortedPosts = [...filteredPosts].sort((a, b) => {
     if (sortOrder === "recent") {
       // 최신순: 생성시간 내림차순 (최근 것이 먼저)
       return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
@@ -57,7 +115,7 @@ export default function CommunityMain() {
   return (
     <>
       <div className="mt-14 mb-[60px]">
-        <SearchInput handleSubmit={() => alert("submit!")} />
+        <SearchInput handleSubmit={(input) => handleSearch(input)} />
       </div>
 
       <div className="flex gap-[50px] justify-between items-center mb-[30px]">
@@ -72,9 +130,24 @@ export default function CommunityMain() {
         </div>
 
         <div className="flex gap-[5px]">
-          <Tag tag="clean" isCheckbox />
-          <Tag tag="kind" isCheckbox />
-          <Tag tag="convenience" isCheckbox />
+          <Tag
+            tag="clean"
+            isCheckbox
+            defaultChecked={tagList.includes("clean")}
+            onChange={(checked: boolean) => handleTagChange("clean", checked)}
+          />
+          <Tag
+            tag="kind"
+            isCheckbox
+            defaultChecked={tagList.includes("kind")}
+            onChange={(checked: boolean) => handleTagChange("kind", checked)}
+          />
+          <Tag
+            tag="convenience"
+            isCheckbox
+            defaultChecked={tagList.includes("convenience")}
+            onChange={(checked: boolean) => handleTagChange("convenience", checked)}
+          />
         </div>
 
         <button
