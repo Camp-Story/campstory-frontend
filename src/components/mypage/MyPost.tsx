fdac8ef25031c719ad5c8abf1ddfcb757b5f0791
marie <marie061313@gmail.com>
2025-02-05 14:59:27 +0900

    feat: 컴포넌트 props 수정

--- src/components/mypage/MyPost.tsx
+++ src/components/mypage/MyPost.tsx
@@ -2,25 +2,29 @@ import { useNavigate } from "react-router";
 import { PATH } from "@constants/path";
 import AreaCard from "@./components/community/community/AreaCard";
 import TagList from "@./components/community/TagList";
+import { Tag } from "@components/community/Tag";
 
-export default function Post() {
+interface PostProps {
+  content: string;
+  tags: Tag[];
+  image: string;
+}
+
+export default function Post({ content, tags, image }: PostProps) {
   const navigate = useNavigate();
 
   return (
     <div onClick={() => navigate(PATH.communityPost("1"))} className="max-w-[420px] cursor-pointer">
       <div className="flex flex-col gap-2.5 mb-[15px]">
         {/* TODO: || "https://placehold.co/490x320?text=CAMP+STORY" */}
-        <img src="/images/community/communityPostItem.png" alt="thumbnail" />
+        <img src={image} alt="thumbnail" />
 
-        <TagList tags={["clean", "kind", "convenience"]} />
+        <TagList tags={tags} />
 
-        <AreaCard location="울산 울주군군" thumbnail="" title="신불산베이스캠프" />
+        <AreaCard location="울산 울주군" thumbnail="" title="신불산베이스캠프" />
       </div>
 
-      <div className="text-[18px] text-gray-scale-400 line-clamp-2">
-        접근성좋고 주변 맛집도 있고 시설도 깨끗하고 좋았어요. 아이들이 놀기에도 좋은거같고 한번간
-        캠핑장은 잘 안가는데 장박하시는 분들도 많이보였어요. 또 갈게요
-      </div>
+      <div className="text-[18px] text-gray-scale-400 line-clamp-2">{content}</div>
     </div>
   );
 }
