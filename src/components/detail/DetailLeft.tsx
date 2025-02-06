8c54521e1f0b278746a1cfd9c9e97d3c4aaa3751
leejihwan99 <lbwlimes@gmail.com>
2025-02-06 11:46:26 +0900

    feat: DetailLeft 컴포넌트에 날씨 섹션 추가 및 날씨 api 연동

--- src/components/detail/DetailLeft.tsx
+++ src/components/detail/DetailLeft.tsx
@@ -1,10 +1,82 @@
+import { useEffect, useState } from "react";
+import WeatherSection from "./WeatherSection";
+import { fetchWeatherData } from "@utils/fetchWeatherData";
+
 interface DetailLeftProps {
   image1: string;
   image2: string;
   image3: string;
+  mapX: string;
+  mapY: string;
 }
 
-export default function DetailLeft({ image1, image2, image3 }: DetailLeftProps) {
+interface WeatherDataType {
+  SKY?: string; // 현재 하늘 상태: 맑음(1), 구름많음(3), 흐림(4)
+  TMP?: string; // 현재 온도
+  TMN?: string; // 최저기온
+  TMX?: string; // 최고기온
+  REH?: string; // 습도
+  POP?: string; // 강수 확률
+  PCP?: string; // 강수량
+  PTY?: string; // 강수형태
+}
+
+interface WeatherDataItem {
+  baseDate: string; // 예보 기준 날짜 (YYYYMMDD)
+  baseTime: string; // 예보 기준 시간 (HHMM)
+  category: string; // 기상 요소 (TMP: 기온, REH: 습도 등)
+  fcstDate: string; // 예측 날짜 (YYYYMMDD)
+  fcstTime: string; // 예측 시간 (HHMM)
+  fcstValue: string; // 예측 값 (예: 온도, 습도 등)
+  nx: number; // 예보 지역 X 좌표
+  ny: number; // 예보 지역 Y 좌표
+}
+
+function getSkyDescription(sky: string | undefined): string {
+  switch (sky) {
+    case "1":
+      return "맑음";
+    case "3":
+      return "구름많음";
+    case "4":
+      return "흐림";
+    default:
+      return "";
+  }
+}
+
+export default function DetailLeft({ image1, image2, image3, mapX, mapY }: DetailLeftProps) {
+  const [weatherData, setWeatherData] = useState<WeatherDataType | null>(null);
+
+  const getWeatherData = async () => {
+    const weatherData: WeatherDataItem[] = await fetchWeatherData(mapX, mapY);
+
+    const sky = weatherData.find((item) => item.category === "SKY")?.fcstValue;
+    const tmp = weatherData.find((item) => item.category === "TMP")?.fcstValue;
+    const tmn = weatherData.find((item) => item.category === "TMN")?.fcstValue;
+    const tmx = weatherData.find((item) => item.category === "TMX")?.fcstValue;
+    const reh = weatherData.find((item) => item.category === "REH")?.fcstValue;
+    const pop = weatherData.find((item) => item.category === "POP")?.fcstValue;
+    const pcp = weatherData.find((item) => item.category === "PCP")?.fcstValue;
+    const pty = weatherData.find((item) => item.category === "PTY")?.fcstValue;
+
+    setWeatherData({
+      SKY: getSkyDescription(sky),
+      TMP: tmp,
+      TMN: tmn,
+      TMX: tmx,
+      REH: reh,
+      POP: pop,
+      PCP: pcp,
+      PTY: pty,
+    });
+    console.log({ sky, tmp, tmn, tmx, reh, pop, pcp, pty });
+  };
+
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+
   return (
     <article className="w-7/12 grid grid-cols-6 gap-5">
       <div className="col-span-6 h-[444px] bg-gray-scale-100 rounded-xl overflow-hidden">
@@ -31,9 +103,7 @@ export default function DetailLeft({ image1, image2, image3 }: DetailLeftProps) 
       <div className="col-span-2 h-[140px] bg-gray-scale-100 rounded-xl flex items-center justify-center text-sub-title">
         더보기
       </div>
-      <div className="col-span-6 h-64 bg-gray-scale-100 rounded-xl overflow-hidden flex items-center justify-center">
-        기능 미정
-      </div>
+      <WeatherSection {...weatherData} />
     </article>
   );
 }
