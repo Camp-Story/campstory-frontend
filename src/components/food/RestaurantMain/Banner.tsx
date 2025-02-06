09f40f39c745ac080964744f0259bf91c5df2603
marie <marie061313@gmail.com>
2025-02-06 13:00:43 +0900

    design: 배너 컴포넌트 마진 제거

--- src/components/food/RestaurantMain/Banner.tsx
+++ src/components/food/RestaurantMain/Banner.tsx
@@ -2,7 +2,7 @@ import SearchBar from "./SearchBar";
 
 export default function Banner() {
   return (
-    <div className="relative mt-[10px]">
+    <div className="relative">
       <img src="/images/food/food-banner.png" alt="메인이미지" className="w-full object-cover" />
       <SearchBar className="absolute bottom-[60px] left-[50%] -translate-x-[50%]" />
     </div>
