db60c067049e6d9cab605500b5eb1678a2a29655
leejihwan99 <lbwlimes@gmail.com>
2025-02-07 10:40:51 +0900

    feat: 캘린더 행사 카드 컴포넌트 테두리 스타일 수정

--- src/components/event/CalendarEventCard.tsx
+++ src/components/event/CalendarEventCard.tsx
@@ -9,7 +9,7 @@ export default function CalendarEventCard({
   return (
     <button
       onClick={handleClick}
-      className="h-[130px] border border-gray-scale-200 px-4 py-2 rounded-xl bg-white drop-shadow-md flex items-center hover:brightness-75"
+      className="h-[130px] border border-gray-scale-100 px-4 py-2 rounded-xl bg-white drop-shadow-md flex items-center hover:brightness-75"
     >
       <div className="w-[100px] h-[100px] mr-4">
         <img src={src} alt="행사 이미지" className="w-full h-full object-cover rounded-lg" />
